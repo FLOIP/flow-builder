c27f3d88d754a08b4fa6c9db3c8941afac5fde16
safydy <r.safydy@gmail.com>
2020-07-27 09:12:34 -0600

    VMO-2632 add other dev dependencies to consider stories

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset',
+  presets: ['@vue/cli-plugin-babel/preset'],
+  plugins: [
+    ['@babel/plugin-proposal-decorators', {legacy: true}],
+    ['@babel/plugin-proposal-class-properties', {loose: true}],
   ],
-};
+}
