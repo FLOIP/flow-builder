15ca7a07e7044acc8d98be0960159395c5a41ff4
safydy <r.safydy@gmail.com>
2020-07-29 12:43:49 -0600

    VMO-2635 update messages.json

--- src/components/interaction-designer/block-editors/MaxDigitEditor.vue
+++ src/components/interaction-designer/block-editors/MaxDigitEditor.vue
@@ -2,7 +2,7 @@
   <div v-if="hasIvr" class="form-group block-max-digits">
     <numeric-editor v-model.number="maxDigits"
         :is-editable="isEditable"
-        :label="'flow-builder.Maximum-digits' | trans"
+        :label="'flow-builder.maximum-digits' | trans"
         :placeholder="'flow-builder.enter-value' | trans">
     </numeric-editor>
   </div>
