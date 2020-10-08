cedd6e40c24705a2551e25c4885fc99d4fbabc59
safydy <r.safydy@gmail.com>
2020-10-07 21:42:50 -0600

    VMO-2642 make NumericResponseBlock working

--- src/stories/NumericResponseBlock.stories.ts
+++ src/stories/NumericResponseBlock.stories.ts
@@ -10,13 +10,11 @@ import numericResponseBlockStore, {BLOCK_TYPE} from '@/store/flow/block-types/Mo
 import {SupportedMode, IFlow, SupportedContentType} from '@floip/flow-runner'
 import {IResourceDefinitionVariantOverModesFilter} from '@/store/flow/resource'
 
-import stubbedFilters from '@/stories/story-utils/stubbedFilters'
 import {baseMounted, BaseMountedVueClass} from '@/stories/story-utils/storeSetup'
 import {Component} from 'vue-property-decorator'
 import {namespace} from 'vuex-class'
 import {get} from 'lodash'
 
-Vue.filter('trans', stubbedFilters.trans)
 Vue.use(Vuex)
 
 const flowVuexNamespace = namespace('flow')
@@ -41,11 +39,8 @@ const BaseOptions = {
   template: NumericResponseBlockTemplate,
 }
 
-class DefaultClass extends BaseMountedVueClass {}
-
 // default numeric-response block state
-export const Default = () => (
-  @Component<any>(
+@Component<any>(
     {
       ...BaseOptions,
       store: new Vuex.Store<IRootState>(store),
@@ -53,198 +48,198 @@ export const Default = () => (
         await baseMounted.bind(this)(BLOCK_TYPE, numericResponseBlockStore)
       },
     }
-  )
-  class CurrentClass1 extends DefaultClass {}
 )
+class CurrentClass1 extends BaseMountedVueClass {}
+export const Default = () => (CurrentClass1)
 
-export const ExistingDataForAllModes = () => (
-  @Component<any>({
-    ...BaseOptions,
-    store: new Vuex.Store<IRootState>(store),
-    async mounted() {
-      const {block: {uuid: blockId}, flow: {uuid: flowId}} = await baseMounted.bind(this)(BLOCK_TYPE, numericResponseBlockStore)
-      const {
-        languages: {
-          0: {id: languageId}
-        },
-      }: IFlow = this.activeFlow
-      const resourceId = get(this.activeBlock, `config.prompt`, '')
+// ExistingDataForAllModes
+@Component<any>({
+  ...BaseOptions,
+  store: new Vuex.Store<IRootState>(store),
+  async mounted() {
+    const {block: {uuid: blockId}, flow: {uuid: flowId}} = await baseMounted.bind(this)(BLOCK_TYPE, numericResponseBlockStore)
+    const {
+      languages: {
+        0: {id: languageId}
+      },
+    }: IFlow = this.activeFlow
+    const resourceId = get(this.activeBlock, `config.prompt`, '')
 
-      this.setDescription(blockId)
-      this.setResourceData(languageId, resourceId)
-      this.setValidationMinimum({blockId, value:0})
-      this.setValidationMaximum({blockId, value:99})
-      this.setMaxDigits({blockId, value:2})
-    },
-  })
-  class CurrentClass2 extends DefaultClass {
+    this.setDescription(blockId)
+    this.setResourceData(languageId, resourceId)
+    this.setValidationMinimum({blockId, value:0})
+    this.setValidationMaximum({blockId, value:99})
+    this.setMaxDigits({blockId, value:2})
+  },
+})
+class CurrentClass2 extends BaseMountedVueClass {
 
-    setDescription(blockId) { // TODO: Find a wait to define this in BaseClass or other ParentClass without '_this.setDescription is not a function' error
-      this.block_setName({blockId: blockId, value: "A Name"})
-      this.block_setLabel({blockId: blockId, value: "A Label"})
-      this.block_setSemanticLabel({blockId: blockId, value: "A Semantic Label"})
-    }
-
-    setResourceData(languageId, resourceId) {
-      // Set values on resource editor // TODO: find better way to do this once the resource editor is fully implemented
-      const variantSms: IResourceDefinitionVariantOverModesFilter = {
-        languageId,
-        modes: ['sms'],
-        contentType: SupportedContentType.TEXT,
-      }
-      const variantUssd: IResourceDefinitionVariantOverModesFilter = {
-        languageId,
-        modes: ['ussd'],
-        contentType: SupportedContentType.TEXT,
-      }
-      const variantIvr: IResourceDefinitionVariantOverModesFilter = {
-        languageId,
-        modes: ['ivr'],
-        contentType: SupportedContentType.TEXT,
-      }
-      // we're assuming this pseudo-variants exist
-      this.resource_setValue({resourceId, filter: variantSms, value: "text for SMS"})
-      this.resource_setValue({resourceId, filter: variantUssd, value: "text for USSD"})
-      this.resource_setValue({resourceId, filter: variantIvr, value: "text for IVR"})
-    }
-    
-    @blockVuexNamespace.Action setValidationMinimum:any
-    @blockVuexNamespace.Action setValidationMaximum:any
-    @blockVuexNamespace.Action setMaxDigits:any
-
-    @flowVuexNamespace.Mutation block_setName:any
-    @flowVuexNamespace.Mutation block_setLabel:any
-    @flowVuexNamespace.Mutation block_setSemanticLabel:any
-    @flowVuexNamespace.Mutation resource_setValue:any
+  setDescription(blockId) { // TODO: Find a wait to define this in BaseClass or other ParentClass without '_this.setDescription is not a function' error
+    this.block_setName({blockId: blockId, value: "A Name"})
+    this.block_setLabel({blockId: blockId, value: "A Label"})
+    this.block_setSemanticLabel({blockId: blockId, value: "A Semantic Label"})
   }
-)
 
-export const ExistingDataForIvrOnly = () => (
-  @Component<any>({
-    ...BaseOptions,
-    store: new Vuex.Store<IRootState>(store),
-    async mounted() {
-      const {block: {uuid: blockId}, flow: {uuid: flowId}} = await baseMounted.bind(this)(BLOCK_TYPE, numericResponseBlockStore)
-      const {
-        languages: {
-          0: {id: languageId}
-        },
-      }: IFlow = this.activeFlow
-      const resourceId = get(this.activeBlock, `config.prompt`, '')
-
-      this.flow_setSupportedMode({flowId, value: SupportedMode.IVR})
-      this.setValidationMinimum({blockId, value:0})
-      this.setValidationMaximum({blockId, value:99})
-      this.setMaxDigits({blockId, value:2})
-      this.setDescription(blockId)
-      this.setResourceData(languageId, resourceId)
-    },
-  })
-  class CurrentClass3 extends DefaultClass {
-    setDescription(blockId) { // TODO: Find a wait to define this in BaseClass or other ParentClass without '_this.setDescription is not a function' error
-      this.block_setName({blockId: blockId, value: "A Name"})
-      this.block_setLabel({blockId: blockId, value: "A Label"})
-      this.block_setSemanticLabel({blockId: blockId, value: "A Semantic Label"})
+  setResourceData(languageId, resourceId) {
+    // Set values on resource editor // TODO: find better way to do this once the resource editor is fully implemented
+    const variantSms: IResourceDefinitionVariantOverModesFilter = {
+      languageId,
+      modes: ['sms'],
+      contentType: SupportedContentType.TEXT,
     }
-
-    setResourceData(languageId, resourceId) {
-      // Set values on resource editor // TODO: find better way to do this once the resource editor is fully implemented
-      const variantSms: IResourceDefinitionVariantOverModesFilter = {
-        languageId,
-        modes: ['sms'],
-        contentType: SupportedContentType.TEXT,
-      }
-      const variantUssd: IResourceDefinitionVariantOverModesFilter = {
-        languageId,
-        modes: ['ussd'],
-        contentType: SupportedContentType.TEXT,
-      }
-      const variantIvr: IResourceDefinitionVariantOverModesFilter = {
-        languageId,
-        modes: ['ivr'],
-        contentType: SupportedContentType.TEXT,
-      }
-      // we're assuming this pseudo-variants exist
-      this.resource_setValue({resourceId, filter: variantSms, value: "text for SMS"})
-      this.resource_setValue({resourceId, filter: variantUssd, value: "text for USSD"})
-      this.resource_setValue({resourceId, filter: variantIvr, value: "text for IVR"})
+    const variantUssd: IResourceDefinitionVariantOverModesFilter = {
+      languageId,
+      modes: ['ussd'],
+      contentType: SupportedContentType.TEXT,
     }
-
-    @blockVuexNamespace.Action setValidationMinimum:any
-    @blockVuexNamespace.Action setValidationMaximum:any
-    @blockVuexNamespace.Action setMaxDigits:any
-
-    @flowVuexNamespace.Mutation block_setName:any
-    @flowVuexNamespace.Mutation block_setLabel:any
-    @flowVuexNamespace.Mutation block_setSemanticLabel:any
-    @flowVuexNamespace.Mutation resource_setValue:any
-    @flowVuexNamespace.Mutation flow_setSupportedMode:any
+    const variantIvr: IResourceDefinitionVariantOverModesFilter = {
+      languageId,
+      modes: ['ivr'],
+      contentType: SupportedContentType.AUDIO,
+    }
+    // we're assuming this pseudo-variants exist
+    this.resource_setValue({resourceId, filter: variantSms, value: "text for SMS"})
+    this.resource_setValue({resourceId, filter: variantUssd, value: "text for USSD"})
+    this.resource_setValue({resourceId, filter: variantIvr, value: "path/to/ivr audio.mp3"})
   }
-)
 
-export const ExistingDataForTextOnly = () => (
-  @Component<any>({
-    ...BaseOptions,
-    store: new Vuex.Store<IRootState>(store),
-    async mounted() {
-      const {block: {uuid: blockId}, flow: {uuid: flowId}} = await baseMounted.bind(this)(BLOCK_TYPE, numericResponseBlockStore)
-      const {
-        languages: {
-          0: {id: languageId}
-        },
-      }: IFlow = this.activeFlow
-      const resourceId = get(this.activeBlock, `config.prompt`, '')
+  @blockVuexNamespace.Action setValidationMinimum:any
+  @blockVuexNamespace.Action setValidationMaximum:any
+  @blockVuexNamespace.Action setMaxDigits:any
 
-      this.flow_setSupportedMode({flowId, value: [SupportedMode.SMS, SupportedMode.USSD]})
-      this.setValidationMinimum({blockId, value:0})
-      this.setValidationMaximum({blockId, value:99})
-      this.setDescription(blockId)
-      this.setResourceData(languageId, resourceId)
-    },
-  })
-  class CurrentClass4 extends DefaultClass {
-    setDescription(blockId) { // TODO: Find a wait to define this in BaseClass or other ParentClass without '_this.setDescription is not a function' error
-      this.block_setName({blockId: blockId, value: "A Name"})
-      this.block_setLabel({blockId: blockId, value: "A Label"})
-      this.block_setSemanticLabel({blockId: blockId, value: "A Semantic Label"})
-    }
+  @flowVuexNamespace.Mutation block_setName:any
+  @flowVuexNamespace.Mutation block_setLabel:any
+  @flowVuexNamespace.Mutation block_setSemanticLabel:any
+  @flowVuexNamespace.Mutation resource_setValue:any
+}
+export const ExistingDataForAllModes = () => (CurrentClass2)
 
-    setResourceData(languageId, resourceId) {
-      // Set values on resource editor // TODO: find better way to do this once the resource editor is fully implemented
-      const variantSms: IResourceDefinitionVariantOverModesFilter = {
-        languageId,
-        modes: ['sms'],
-        contentType: SupportedContentType.TEXT,
-      }
-      const variantUssd: IResourceDefinitionVariantOverModesFilter = {
-        languageId,
-        modes: ['ussd'],
-        contentType: SupportedContentType.TEXT,
-      }
-      const variantIvr: IResourceDefinitionVariantOverModesFilter = {
-        languageId,
-        modes: ['ivr'],
-        contentType: SupportedContentType.TEXT,
-      }
-      // we're assuming this pseudo-variants exist
-      this.resource_setValue({resourceId, filter: variantSms, value: "text for SMS"})
-      this.resource_setValue({resourceId, filter: variantUssd, value: "text for USSD"})
-      this.resource_setValue({resourceId, filter: variantIvr, value: "text for IVR"})
-    }
+// ExistingDataForIvrOnly
+@Component<any>({
+  ...BaseOptions,
+  store: new Vuex.Store<IRootState>(store),
+  async mounted() {
+    const {block: {uuid: blockId}, flow: {uuid: flowId}} = await baseMounted.bind(this)(BLOCK_TYPE, numericResponseBlockStore)
+    const {
+      languages: {
+        0: {id: languageId}
+      },
+    }: IFlow = this.activeFlow
+    const resourceId = get(this.activeBlock, `config.prompt`, '')
 
-    @blockVuexNamespace.Action setValidationMinimum:any
-    @blockVuexNamespace.Action setValidationMaximum:any
-
-    @flowVuexNamespace.Mutation block_setName:any
-    @flowVuexNamespace.Mutation block_setLabel:any
-    @flowVuexNamespace.Mutation block_setSemanticLabel:any
-    @flowVuexNamespace.Mutation resource_setValue:any
-    @flowVuexNamespace.Mutation flow_setSupportedMode:any
+    this.flow_setSupportedMode({flowId, value: SupportedMode.IVR})
+    this.setValidationMinimum({blockId, value:0})
+    this.setValidationMaximum({blockId, value:99})
+    this.setMaxDigits({blockId, value:2})
+    this.setDescription(blockId)
+    this.setResourceData(languageId, resourceId)
+  },
+})
+class CurrentClass3 extends BaseMountedVueClass {
+  setDescription(blockId) { // TODO: Find a wait to define this in BaseClass or other ParentClass without '_this.setDescription is not a function' error
+    this.block_setName({blockId: blockId, value: "A Name"})
+    this.block_setLabel({blockId: blockId, value: "A Label"})
+    this.block_setSemanticLabel({blockId: blockId, value: "A Semantic Label"})
   }
-)
 
-export const NonStartingBlock = () => (
-  @Component<any>(
+  setResourceData(languageId, resourceId) {
+    // Set values on resource editor // TODO: find better way to do this once the resource editor is fully implemented
+    const variantSms: IResourceDefinitionVariantOverModesFilter = {
+      languageId,
+      modes: ['sms'],
+      contentType: SupportedContentType.TEXT,
+    }
+    const variantUssd: IResourceDefinitionVariantOverModesFilter = {
+      languageId,
+      modes: ['ussd'],
+      contentType: SupportedContentType.TEXT,
+    }
+    const variantIvr: IResourceDefinitionVariantOverModesFilter = {
+      languageId,
+      modes: ['ivr'],
+      contentType: SupportedContentType.AUDIO,
+    }
+    // we're assuming this pseudo-variants exist
+    this.resource_setValue({resourceId, filter: variantSms, value: "text for SMS"})
+    this.resource_setValue({resourceId, filter: variantUssd, value: "text for USSD"})
+    this.resource_setValue({resourceId, filter: variantIvr, value: "path/to/ivr audio.mp3"})
+  }
+
+  @blockVuexNamespace.Action setValidationMinimum:any
+  @blockVuexNamespace.Action setValidationMaximum:any
+  @blockVuexNamespace.Action setMaxDigits:any
+
+  @flowVuexNamespace.Mutation block_setName:any
+  @flowVuexNamespace.Mutation block_setLabel:any
+  @flowVuexNamespace.Mutation block_setSemanticLabel:any
+  @flowVuexNamespace.Mutation resource_setValue:any
+  @flowVuexNamespace.Mutation flow_setSupportedMode:any
+}
+export const ExistingDataForIvrOnly = () => (CurrentClass3)
+
+//ExistingDataForTextOnly
+@Component<any>({
+  ...BaseOptions,
+  store: new Vuex.Store<IRootState>(store),
+  async mounted() {
+    const {block: {uuid: blockId}, flow: {uuid: flowId}} = await baseMounted.bind(this)(BLOCK_TYPE, numericResponseBlockStore)
+    const {
+      languages: {
+        0: {id: languageId}
+      },
+    }: IFlow = this.activeFlow
+    const resourceId = get(this.activeBlock, `config.prompt`, '')
+
+    this.flow_setSupportedMode({flowId, value: [SupportedMode.SMS, SupportedMode.USSD]})
+    this.setValidationMinimum({blockId, value:0})
+    this.setValidationMaximum({blockId, value:99})
+    this.setDescription(blockId)
+    this.setResourceData(languageId, resourceId)
+  },
+})
+class CurrentClass4 extends BaseMountedVueClass {
+  setDescription(blockId) { // TODO: Find a wait to define this in BaseClass or other ParentClass without '_this.setDescription is not a function' error
+    this.block_setName({blockId: blockId, value: "A Name"})
+    this.block_setLabel({blockId: blockId, value: "A Label"})
+    this.block_setSemanticLabel({blockId: blockId, value: "A Semantic Label"})
+  }
+
+  setResourceData(languageId, resourceId) {
+    // Set values on resource editor // TODO: find better way to do this once the resource editor is fully implemented
+    const variantSms: IResourceDefinitionVariantOverModesFilter = {
+      languageId,
+      modes: ['sms'],
+      contentType: SupportedContentType.TEXT,
+    }
+    const variantUssd: IResourceDefinitionVariantOverModesFilter = {
+      languageId,
+      modes: ['ussd'],
+      contentType: SupportedContentType.TEXT,
+    }
+    const variantIvr: IResourceDefinitionVariantOverModesFilter = {
+      languageId,
+      modes: ['ivr'],
+      contentType: SupportedContentType.AUDIO,
+    }
+    // we're assuming this pseudo-variants exist
+    this.resource_setValue({resourceId, filter: variantSms, value: "text for SMS"})
+    this.resource_setValue({resourceId, filter: variantUssd, value: "text for USSD"})
+    this.resource_setValue({resourceId, filter: variantIvr, value: "path/to/ivr audio.mp3"})
+  }
+
+  @blockVuexNamespace.Action setValidationMinimum:any
+  @blockVuexNamespace.Action setValidationMaximum:any
+
+  @flowVuexNamespace.Mutation block_setName:any
+  @flowVuexNamespace.Mutation block_setLabel:any
+  @flowVuexNamespace.Mutation block_setSemanticLabel:any
+  @flowVuexNamespace.Mutation resource_setValue:any
+  @flowVuexNamespace.Mutation flow_setSupportedMode:any
+}
+export const ExistingDataForTextOnly = () => (CurrentClass4)
+
+//NonStartingBlock
+@Component<any>(
     {
       ...BaseOptions,
       store: new Vuex.Store<IRootState>(store),
@@ -263,11 +258,11 @@ export const NonStartingBlock = () => (
         this.flow_setFirstBlockId({blockId: caseBlockId, flowId: flowId})
       },
     }
-  )
-  class CurrentClass5 extends DefaultClass {
-    @flowVuexNamespace.Mutation block_setName:any
-    @flowVuexNamespace.Mutation block_setLabel:any
-    @flowVuexNamespace.Mutation block_setSemanticLabel:any
-    @flowVuexNamespace.Mutation flow_setFirstBlockId:any
-  }
 )
+class CurrentClass5 extends BaseMountedVueClass {
+  @flowVuexNamespace.Mutation block_setName:any
+  @flowVuexNamespace.Mutation block_setLabel:any
+  @flowVuexNamespace.Mutation block_setSemanticLabel:any
+  @flowVuexNamespace.Mutation flow_setFirstBlockId:any
+}
+export const NonStartingBlock = () => (CurrentClass5)
