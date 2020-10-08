17ca7533bff4982034546e38867ae11752648d76
safydy <r.safydy@gmail.com>
2020-10-07 23:28:46 -0600

    VMO-2642 refactor codes for CaseBlockStyled.stories.ts

--- src/stories/CaseBlockStyled.stories.ts
+++ src/stories/CaseBlockStyled.stories.ts
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import Vuex, {mapActions, mapGetters, mapMutations} from 'vuex'
+import Vuex from 'vuex'
 import {Component} from 'vue-property-decorator'
 
 import CaseBlock from '@/components/interaction-designer/block-types/Core_CaseBlock.vue'
@@ -9,6 +9,9 @@ import {IRootState, store} from '@/store'
 import caseBlockStore, {BLOCK_TYPE} from '@/store/flow/block-types/Core_CaseBlockStore'
 
 import { baseMounted, BaseMountedVueClass } from '@/stories/story-utils/storeSetup'
+import {namespace} from "vuex-class";
+const flowVuexNamespace = namespace('flow')
+const blockVuexNamespace = namespace(`flow/${BLOCK_TYPE}`)
 
 Vue.use(Vuex)
 
@@ -26,10 +29,14 @@ const CaseBlockTemplate = `
   </flow-builder-sidebar-editor-container>
 `
 
-// default case block state
-@Component<any>({
+const BaseOptions = {
   components: {CaseBlock, FlowBuilderSidebarEditorContainer},
   template: CaseBlockTemplate,
+}
+
+// default case block state
+@Component<any>({
+  ...BaseOptions,
   store: new Vuex.Store<IRootState>(store),
   async mounted() {
     await baseMounted.bind(this)(BLOCK_TYPE, caseBlockStore)
@@ -38,9 +45,9 @@ const CaseBlockTemplate = `
 class DefaultClass extends BaseMountedVueClass {}
 export const Default = () => (DefaultClass)
 
-export const ExistingDataBlock = () => ({
-  components: {CaseBlock, FlowBuilderSidebarEditorContainer},
-  template: CaseBlockTemplate,
+//ExistingDataBlock
+@Component<any>({
+  ...BaseOptions,
   store: new Vuex.Store<IRootState>(store),
   async mounted() {
     const { block } = await baseMounted.bind(this)(BLOCK_TYPE, caseBlockStore)
@@ -54,31 +61,13 @@ export const ExistingDataBlock = () => ({
     this.block_setSemanticLabel({blockId: blockId, value: "A Semantic Label"})
     // @ts-ignore - TS2339: Property 'editCaseBlockExit' does not exist on type
     this.editCaseBlockExit({identifier: block.exits[0].uuid, value: "A expression"})
-  },
-
-  computed: {
-    ...mapGetters('flow', [
-      'activeFlow',
-    ]),
-    ...mapGetters('builder', [
-      'activeBlock',
-    ]),
-  },
-
-  methods: {
-    ...mapMutations('flow', [
-      'block_setName',
-      'block_setLabel',
-      'block_setSemanticLabel',
-    ]),
-    ...mapMutations('builder', [
-      'activateBlock',
-    ]),
-    ...mapActions('flow', [
-      'flow_addBlankFlow',
-      'flow_addBlankBlockByType']),
-    ...mapActions(`flow/${BLOCK_TYPE}`, [
-      'editCaseBlockExit'
-    ]),
   }
 })
+class CurrentClass2 extends BaseMountedVueClass {
+  @blockVuexNamespace.Action editCaseBlockExit:any
+
+  @flowVuexNamespace.Mutation block_setName:any
+  @flowVuexNamespace.Mutation block_setLabel:any
+  @flowVuexNamespace.Mutation block_setSemanticLabel:any
+}
+export const ExistingDataBlock = () => (CurrentClass2)
