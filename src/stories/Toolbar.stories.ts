4637c0e3efed2916de530fac7a7aed0717439ca9
safydy <r.safydy@gmail.com>
2020-10-08 21:53:55 -0600

    VMO-2926 add edit flow story

--- src/stories/Toolbar.stories.ts
+++ src/stories/Toolbar.stories.ts
@@ -1,7 +1,7 @@
 import Vuex from 'vuex'
 import Vue from 'vue'
 import {
-    Action,
+    Action, Mutation
 } from 'vuex-class'
 import FlowBuilderContainer from "@/stories/story-utils/FlowBuilderContainer.vue";
 import {Component} from 'vue-property-decorator'
@@ -9,10 +9,9 @@ import {IRootState, store} from '@/store'
 import TreeBuilderToolbar from "@/components/interaction-designer/toolbar/TreeBuilderToolbar.vue";
 
 import {namespace} from 'vuex-class'
-const flowVuexNamespace = namespace('flow')
 
 export default {
-  title: 'Toolbar/Toolbar slot extension',
+  title: 'InteractionDesigner/Toolbar',
   // Our exports that end in "Data" are not stories.
   excludeStories: /.*Data$/,
 }
@@ -39,47 +38,26 @@ class BaseMountedClass extends Vue {
 @Component<any>(
     {
         ...BaseOptions,
+      async mounted() {
+        this.updateIsEditable({value: 0})
+      }
     }
 )
-class CurrentClass extends BaseMountedClass {}
+class CurrentClass extends BaseMountedClass {
+  @Mutation updateIsEditable: any
+}
 export const Default = () => (CurrentClass)
 
-//ExistingDataPreFilled
-// @Component<any>(
-//     {
-//         ...BaseOptions,
-//         store: new Vuex.Store<IRootState>(store),
-//         async mounted() {
-//             const {uuid: flowId} = await this.flow_addBlankFlow()
-//             const sampleLanguages: ILanguage[] = [
-//                 {
-//                     id: '1',
-//                     name: "English",
-//                     abbreviation: "EN",
-//                     orgId: "",
-//                     rightToLeft: true,
-//                 },
-//             ]
-//
-//             const sampleModes = [
-//                 SupportedMode.SMS,
-//                 SupportedMode.IVR,
-//                 SupportedMode.RICH_MESSAGING
-//             ]
-//
-//             this.flow_setName({flowId, value: "FlowName"})
-//             this.flow_setLabel({flowId, value: "A flow label"})
-//             this.flow_setInteractionTimeout({flowId, value: 20})
-//             this.flow_setSupportedMode({flowId, value: sampleModes})
-//             this.flow_setLanguages({flowId, value: sampleLanguages})
-//         },
-//     }
-// )
-// class CurrentClass2 extends BaseMountedVueClass {
-//     @flowVuexNamespace.Mutation flow_setName:any
-//     @flowVuexNamespace.Mutation flow_setLabel:any
-//     @flowVuexNamespace.Mutation flow_setInteractionTimeout:any
-//     @flowVuexNamespace.Mutation flow_setSupportedMode:any
-//     @flowVuexNamespace.Mutation flow_setLanguages:any
-// }
-// export const ExistingDataPreFilled = () => (CurrentClass2)
+// Edit flow
+@Component<any>(
+  {
+    ...BaseOptions,
+    async mounted() {
+      this.updateIsEditable({value: 1})
+    }
+  }
+)
+class CurrentClass2 extends BaseMountedClass {
+  @Mutation updateIsEditable: any
+}
+export const EditFlow = () => (CurrentClass2)
