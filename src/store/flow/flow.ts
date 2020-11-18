90f79ab808d16d9d5d279d70d363bd7f714daee8
safydy <r.safydy@gmail.com>
2020-11-18 13:14:07 -0600

    VMO-3090 put back validationException

--- src/store/flow/flow.ts
+++ src/store/flow/flow.ts
@@ -85,7 +85,9 @@ export const mutations: MutationTree<IFlowsState> = {
   },
 
   flow_setFirstBlockId(state, {flowId, blockId}) {
-    findFlowWith(flowId, state as unknown as IContext).firstBlockId = blockId
+    const flow: IFlow = findFlowWith(flowId, state as unknown as IContext)
+    const block: IBlock = findBlockWith(blockId, flow)  // @throws ValidationException when block absent
+    Vue.set(flow, 'firstBlockId', block.uuid)
   },
   flow_setName(state, {flowId, value}) {
     findFlowWith(flowId, state as unknown as IContext).name = value
