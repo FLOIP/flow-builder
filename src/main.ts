ee74d5847c28e3d6f97f210f3fba49d957745c2e
bzabos <b.zabos@gmail.com>
2020-12-11 01:49:03 -0600

    VMO-2943 Enclose jq+bootstrap deps in async function to stabilize preload sequence.

--- src/main.ts
+++ src/main.ts
@@ -3,23 +3,34 @@ import Vuex from 'vuex'
 import App from './App.vue'
 import router from './router'
 
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/css/bootstrap-theme.css";
-
 Vue.use(Vuex)
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store: new Vuex.Store({}),
-  render: (h) => h(App),
-}).$mount('#app')
-
-// required inline due to front-loading of imports and having jQuery dependency
-window.onload = function () {
-  import('jquery')
-    .then((jQuery) => {
-      import('bootstrap')
-    })
+function exportAsGlobal(exports) {
+  Object.assign(window, exports)
 }
+
+async function initializeBootstrapFramework() {
+  import('bootstrap/dist/css/bootstrap.css')
+  import('bootstrap/dist/css/bootstrap-theme.css')
+
+  const {default: ImportedJquery} = await import('jquery')
+  exportAsGlobal({
+    $: ImportedJquery,
+    jQuery: ImportedJquery})
+
+  await import('bootstrap')
+}
+
+async function main() {
+  await initializeBootstrapFramework()
+
+  new Vue({
+    router,
+    store: new Vuex.Store({}),
+    render: (h) => h(App),
+  }).$mount('#app')
+}
+
+main()
