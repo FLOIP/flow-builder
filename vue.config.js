3162a5c0ad267cef8c323722efd2d8e1f5c1b7c5
bzabos <b.zabos@gmail.com>
2021-01-08 03:11:44 -0600

    DSN-75 Add notes for 'dumb' gh-pages deploy.

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
   lintOnSave: false,
@@ -18,24 +18,40 @@ module.exports = {
   devServer: {
     before(app) {
       // Mock a route to mimic this upload result format:
-      // {"audio_file_id":147,"duration_seconds":"4.803250","description":"xyz.wav","created_at":{"date":"2020-11-24 01:41:58","timezone_type":3,"timezone":"UTC"},"audio_uuid":"5fbc64e0c74e90.82972899"}"
+      // {
+      //   'audio_file_id': 147,
+      //   'duration_seconds': '4.803250',
+      //   'description': 'xyz.wav',
+      //   'created_at': {
+      //     'date': '2020-11-24 01:41:58',
+      //     'timezone_type': 3,
+      //     'timezone': 'UTC'
+      //   },
+      //   'audio_uuid': '5fbc64e0c74e90.82972899'
+      // }
       app.all('/audiofiles/upload', (req, res) => {
-        const now = new Date().toISOString().split('T')
+        const now = new Date()
+          .toISOString()
+          .split('T');
+
         const result = {
           audio_file_id: Math.floor(Math.random() * (1000 + 1)),
           duration_seconds: Math.random() * 10,
           description: req.query.flowFilename,
           created_at: {
             date: `${now[0]} ${now[1].split('.')[0]}`,
-            timezone_type:3,
-            timezone: "UTC"
+            timezone_type: 3,
+            timezone: 'UTC',
           },
-          audio_uuid: `${Math.random().toString(36).substr(2, 16)}.${Math.random().toString(36).substr(2, 10)}`
-        }
+          audio_uuid: `${Math.random()
+            .toString(36)
+            .substr(2, 16)}.${Math.random()
+            .toString(36)
+            .substr(2, 10)}`,
+        };
         res.writeHead(200, { 'Content-Type': 'application/json' });
         res.end(JSON.stringify(result));
       });
     },
-  }
-};
-
+  },
+}
