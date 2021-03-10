c1262aa904b6fc50de1da4c8db6aa43c8052eba1
safydy <r.safydy@gmail.com>
2021-03-10 17:23:43 -0600

    VMO-3601 add preparation script before building for gh-pages

---
+++ scripts/prepare-gh-pages-deployment.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This apply needed changes before running yarn build,
+# as described in https://github.com/FLOIP/flow-builder/blob/master/gh-pages-deploy.md
+
+git apply patches/gh-pages-prepare-router.patch
+git apply patches/gh-pages-prepare-vue-config.patch
