bee1aa6c941d37ab6ba3270da8037d0072f5d959
safydy <r.safydy@gmail.com>
2021-03-10 18:51:26 -0600

    VMO-3601 test2 - push on VMO-3601-github-pages-auto-rebuild branch

--- scripts/prepare-gh-pages-deployment.sh
+++ scripts/prepare-gh-pages-deployment.sh
@@ -3,5 +3,6 @@
 # This apply needed changes before running yarn build,
 # as described in https://github.com/FLOIP/flow-builder/blob/master/gh-pages-deploy.md
 
+echo "Prepare gh-pages deployment"
 git apply patches/gh-pages-prepare-router.patch
 git apply patches/gh-pages-prepare-vue-config.patch
