b46996ae455721c5adcb3c596c438b9676841282
safydy <r.safydy@gmail.com>
2021-03-15 13:46:10 -0600

    VMO-2296 prepare setContactPropertyBlock

---
+++ src/components/interaction-designer/block-types/Core_SetContactPropertyBlock.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <h3 class="no-room-above">
+      {{ 'flow-builder.edit-block-type' | trans({ block_type: trans(`flow-builder.${block.type}`) }) }}
+    </h3>
+
+    <block-name-editor :block="block"/>
+    <block-label-editor :block="block"/>
+    <block-semantic-label-editor :block="block"/>
+
+    <expression-editor :label="'flow-builder.contact-property-expression' | trans"
+                       :placeholder="'flow-builder.edit-expression' | trans"
+                       :current-expression="propertyValue"
+                       @commitExpressionChange="commitExpressionChange"/>
+
+    <first-block-editor-button
+        :flow="flow"
+        :block-id="block.uuid"/>
+
+    <block-id :block="block"/>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { namespace } from 'vuex-class'
+import { Component, Prop } from 'vue-property-decorator'
+
+import {
+  IFlow,
+  IBlock,
+} from '@floip/flow-runner'
+import ExpressionEditor from '@/components/common/ExpressionEditor.vue'
+import BlockNameEditor from '../block-editors/NameEditor.vue'
+import BlockLabelEditor from '../block-editors/LabelEditor.vue'
+import BlockSemanticLabelEditor from '../block-editors/SemanticLabelEditor.vue'
+import FirstBlockEditorButton from '../flow-editors/FirstBlockEditorButton.vue'
+import BlockId from '../block-editors/BlockId.vue'
+
+import SetContactPropertyStore, { BLOCK_TYPE } from '@/store/flow/block-types/Core_SetContactPropertyStore'
+import lang from '@/lib/filters/lang'
+import { createDefaultBlockTypeInstallerFor } from "@/store/builder";
+
+const blockVuexNamespace = namespace(`flow/${BLOCK_TYPE}`)
+
+//providing this generic is required by tsserver checking but not in the build run by yarn storybook
+//TODO - understand what is going on here and if there is something more correct we should have instead
+@Component<any>({
+  components: {
+    ExpressionEditor,
+    BlockNameEditor,
+    BlockLabelEditor,
+    BlockSemanticLabelEditor,
+    FirstBlockEditorButton,
+    BlockId,
+  },
+  mixins: [lang],
+})
+class Core_SetContactPropertyBlock extends Vue {
+  @Prop() readonly block!: IBlock
+  @Prop() readonly flow!: IFlow
+
+  get propertyValue(): string {
+    return this.block.config.set_contact_property.property_value || ''
+  }
+
+  @blockVuexNamespace.Action editSetContactPropertyExpression!: (params: { blockId: string, value: string }) => Promise<string>
+
+  commitExpressionChange(value: string): Promise<string> {
+    return this.editSetContactPropertyExpression({ blockId: this.block.uuid, value })
+  }
+}
+
+export default Core_SetContactPropertyBlock
+export const install = createDefaultBlockTypeInstallerFor(BLOCK_TYPE, SetContactPropertyStore)
+</script>
---
+++ src/store/flow/block-types/Core_SetContactPropertyStore.ts
@@ -0,0 +1,63 @@
+import {ActionTree, GetterTree, MutationTree} from 'vuex'
+import {IRootState} from '@/store'
+import {
+  IBlockExit,
+  ISetContactPropertyBlockConfig
+} from '@floip/flow-runner'
+import {IdGeneratorUuidV4} from '@floip/flow-runner/dist/domain/IdGeneratorUuidV4'
+import {defaults} from 'lodash'
+import {IFlowsState} from '../index'
+
+export const BLOCK_TYPE = 'Core\\SetContactProperty'
+
+export const getters: GetterTree<IFlowsState, IRootState> = {}
+
+export const mutations: MutationTree<IFlowsState> = {}
+
+export const actions: ActionTree<IFlowsState, IRootState> = {
+  async editSetContactPropertyExpression({commit}, {blockId, value}: { blockId: string; value: string }): Promise<string> {
+    const newConfig = {
+      property_value: value
+    }
+    commit('flow/block_updateConfigByKey', {
+      blockId: blockId,
+      key: 'set_contact_property',
+      value: newConfig
+    }, {root: true})
+
+    return value
+  },
+  async createWith({dispatch}, {props}: { props: { uuid: string } & Partial<ISetContactPropertyBlockConfig> }) {
+    const exits: IBlockExit[] = [
+      await dispatch('flow/block_createBlockDefaultExitWith', {
+        props: ({
+          uuid: (new IdGeneratorUuidV4()).generate(),
+          tag: 'Default',
+          label: 'Default',
+        }) as IBlockExit,
+      }, {root: true}),
+    ]
+
+    return defaults(props, {
+      type: BLOCK_TYPE,
+      name: '',
+      label: '',
+      semanticLabel: '',
+      config: {
+        set_contact_property: {
+          property_key: '',
+          property_value: ''
+        },
+      },
+      exits,
+    })
+  },
+
+}
+
+export default {
+  namespaced: true,
+  getters,
+  mutations,
+  actions,
+}
