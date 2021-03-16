0b05d4d080562f6f3f0e7410205b305464890c52
safydy <r.safydy@gmail.com>
2021-03-16 12:04:39 -0600

    VMO-3602 add localization & tooltips on toolbar buttons

--- src/components/interaction-designer/toolbar/TreeBuilderToolbar.vue
+++ src/components/interaction-designer/toolbar/TreeBuilderToolbar.vue
@@ -119,6 +119,7 @@
 
           <button v-if="ui.isEditable"
                   type="button"
+                  v-b-tooltip.hover="trans('flow-builder.tooltip-duplicate-block')"
                   class="btn btn-outline-secondary tree-duplicate-block mr-2"
                   @click.prevent="handleDuplicateActivatedBlockTriggered"
                   :disabled="!activeBlockId">
@@ -127,6 +128,7 @@
 
           <button v-if="ui.isEditable"
                   type="button"
+                  v-b-tooltip.hover="trans('flow-builder.tooltip-delete-block')"
                   class="btn btn-outline-secondary tree-delete-block mr-2"
                   @click.prevent="handleRemoveActivatedBlockTriggered"
                   :disabled="!activeBlockId">
@@ -155,6 +157,7 @@
 
 </template>
 <script lang="ts">
+  import Vue from 'vue'
   import lang from '@/lib/filters/lang'
   import Permissions from '@/lib/mixins/Permissions'
   import Routes from '@/lib/mixins/Routes'
@@ -167,6 +170,9 @@
   // import InteractionTotalsDateRangeConfiguration from './InteractionTotalsDateRangeConfiguration'
   import convertKeysCase from '@/store/flow/utils/DataObjectPropertyNameCaseConverter'
   import {computeBlockPositionsFrom} from '@/store/builder'
+  import {VBTooltipPlugin} from 'bootstrap-vue'
+
+  Vue.use(VBTooltipPlugin)
 
   export default {
     components: {
