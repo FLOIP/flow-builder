039db2fd405a2f58ee71f065224bb89e105c36c9
safydy <r.safydy@gmail.com>
2021-03-19 14:14:44 -0600

    VMO-3601 add comment to help resolve possible conflicts on patches

--- scripts/prepare-gh-pages-deployment.sh
+++ scripts/prepare-gh-pages-deployment.sh
@@ -4,5 +4,9 @@
 # as described in https://github.com/FLOIP/flow-builder/blob/master/gh-pages-deploy.md
 
 echo "Prepare gh-pages deployment"
+# We're applying patches to temporary edit few file during the deployment
+# In case of conflict, we need to regenerate those patches
+# - git diff src/router/index.ts > patches/gh-pages-prepare-router.patch
+# - git diff vue.config.js > patches/gh-pages-prepare-vue-config.patch
 git apply patches/gh-pages-prepare-router.patch
 git apply patches/gh-pages-prepare-vue-config.patch
