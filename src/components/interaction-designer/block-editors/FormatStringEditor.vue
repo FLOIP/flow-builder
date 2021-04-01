113a70f16f95903544039f80d90cdd1352f0dd91
Jacob Ashdown <jcbashdown@gmail.com>
2021-04-01 20:16:36 +0200

    VMO-3162 - validate format strings properly

--- src/components/interaction-designer/block-editors/FormatStringEditor.vue
+++ src/components/interaction-designer/block-editors/FormatStringEditor.vue
@@ -3,12 +3,16 @@
     <text-editor v-model="formatString"
         :label="'flow-builder.format-string' | trans"
         :placeholder="'flow-builder.enter-format-string' | trans"/>
+    <small class="text-muted" v-if="!isScanfSelectorValid">
+      {{'flow-builder.scanf-format-string-invalid' | trans}}
+    </small>
   </div>
 </template>
 
 <script>
 import TextEditor from '@/components/common/TextEditor'
 import lang from '@/lib/filters/lang'
+import scanf from 'scanf'
 
 export default {
   components: {
@@ -21,13 +25,24 @@ export default {
       required: true,
     },
   },
+  data() {
+    return {
+      isScanfSelectorValid: true,
+    }
+  },
   computed: {
     formatString: {
       get() {
         return this.block.config.format_string
       },
-      set(value) {
-        this.$emit('commitFormatStringChange', value)
+      set(newFormatString) {
+        try {
+          scanf(newFormatString)
+          this.isScanfSelectorValid = true
+        } catch (error) {
+          this.isScanfSelectorValid = false
+        }
+        this.$emit('commitFormatStringChange', newFormatString)
       },
     },
   },
