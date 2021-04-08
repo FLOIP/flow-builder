326b4148a04abe0d8907a9ddf5b96c602ff45c18
safydy <r.safydy@gmail.com>
2021-04-08 17:22:54 -0600

    VMO-2297 use defaultsDeep within `createWith()`

--- src/store/flow/block-types/Core_SetContactPropertyStore.ts
+++ src/store/flow/block-types/Core_SetContactPropertyStore.ts
@@ -5,7 +5,7 @@ import {
   ISetContactPropertyBlockConfig
 } from '@floip/flow-runner'
 import {IdGeneratorUuidV4} from '@floip/flow-runner/dist/domain/IdGeneratorUuidV4'
-import {defaults} from 'lodash'
+import {defaultsDeep} from 'lodash'
 import {IFlowsState} from '../index'
 
 export const BLOCK_TYPE = 'Core\\SetContactProperty'
@@ -35,7 +35,7 @@ export const actions: ActionTree<IFlowsState, IRootState> = {
       }, {root: true}),
     ]
 
-    return defaults(props, {
+    return defaultsDeep(props, {
       type: BLOCK_TYPE,
       name: '',
       label: '',
--- src/store/flow/block-types/Core_SetGroupMembershipStore.ts
+++ src/store/flow/block-types/Core_SetGroupMembershipStore.ts
@@ -5,7 +5,7 @@ import {
   ISetGroupMembershipBlockConfig,
 } from '@floip/flow-runner'
 import { IdGeneratorUuidV4 } from '@floip/flow-runner/dist/domain/IdGeneratorUuidV4'
-import { defaults } from 'lodash'
+import { defaultsDeep } from 'lodash'
 import { IFlowsState } from '../index'
 
 export const ADD_KEY = 'add'
@@ -29,7 +29,7 @@ export const actions: ActionTree<IFlowsState, IRootState> = {
       }, { root: true }),
     ]
 
-    return defaults(props, {
+    return defaultsDeep(props, {
       type: BLOCK_TYPE,
       name: '',
       label: '',
