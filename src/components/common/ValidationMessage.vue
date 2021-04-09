ec0c923e2ccd40b54817484bfe1517943e12f432
safydy <r.safydy@gmail.com>
2021-04-09 17:14:45 -0600

    VMO-3704 prepare ValidationMessage.vue

---
+++ src/components/common/ValidationMessage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="validation-message">
+    <b-alert
+      variant="warning"
+      fade
+      :show="showMessage"
+    >
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad commodi,
+    </b-alert>
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Prop} from 'vue-property-decorator';
+import { mixins } from 'vue-class-component';
+import { Lang } from "@/lib/filters/lang";
+import { BAlert } from 'bootstrap-vue'
+
+@Component({
+  components: {
+    BAlert
+  }
+})
+class ValidationMessage extends mixins(Lang) {
+  @Prop() messageKey!: string
+  @Prop() showFixAction?: boolean
+
+  showMessage = true
+}
+export default ValidationMessage
+</script>
