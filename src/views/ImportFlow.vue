86f9641482a7317a91d3d17d2b08a23e828a52eb
Jacob Ashdown <jcbashdown@gmail.com>
2021-04-13 17:35:24 +0200

    VMO-690 - WIP - starting on language etc. matching

--- src/views/ImportFlow.vue
+++ src/views/ImportFlow.vue
@@ -17,23 +17,38 @@
               <input type="radio" value="upload" v-model="uploadOrPaste"> {{'flow-builder.import-json-file' | trans}}
             </label>
             <br>
+            <div v-if="uploadOrPaste === 'upload'">
+              <input type="file"
+                class="btn btn-default"
+                @change="handleFileUpload">
+                  {{'flow-builder.import-file' | trans}}
+              </input>
+              <text-editor v-model="flowJson"
+                v-if="flowJsonText"
+                label=""
+                :placeholder="'flow-builder.edit-flow-json' | trans">
+              </text-editor>
+            </div>
+
+            <br>
             <label class="mt-2 no-weight">
               <input type="radio" value="paste" v-model="uploadOrPaste"> {{'flow-builder.paste-json-directly' | trans}}
             </label>
             <text-editor v-model="flowJson"
                 v-if="uploadOrPaste === 'paste'"
+                label=""
                 :placeholder="'flow-builder.paste-flow-json' | trans">
             </text-editor>
 
             <div class="mt-5 float-right">
               <a :href="route('trees.editTree', {treeId: flowUUID, component: 'interaction-designer', mode: 'edit'})"
                 class="btn btn-primary"
-                :class="{'disabled': !flowUUID}"
+                :class="{'disabled': !flowUUID || flowError}"
                 @click.prevent="handleImportFlow(route('trees.editTree', {treeId: flowUUID, component: 'interaction-designer', mode: 'edit'}))">
                 {{'flow-builder.save-and-continue' | trans}}
               </a>
             </div>
-            //cancel button - router link bank or home is default
+            //cancel button - router link back or home is default
           </div>
         </div>
       </div>
@@ -52,6 +67,7 @@ import { forEach, isEmpty, get } from 'lodash'
 import {store} from '@/store'
 const flowVuexNamespace = namespace('flow')
 import {IFlow, IContext} from '@floip/flow-runner'
+import { ILanguage } from '@floip/flow-runner/dist/flow-spec/ILanguage'
 
 import TextEditor from '@/components/common/TextEditor.vue'
 
@@ -71,20 +87,67 @@ import TextEditor from '@/components/common/TextEditor.vue'
         this.configure({ appConfig: this.appConfig, builderConfig: this.builderConfig })
       }
     },
+    async mounted() {
+      //if(!this.activeFlowContainer) {
+        //this.addEmptyFlowContainer()
+        //langs, groups, props come from builder.config.json
+        //flow or flow container imported?
+        //can match against langs, groups and props
+        //Do resolve ui when conflict
+        //What do we do for lang matching in transfer contact?
+      //}
+    }
   },
 )
 class ImportFlow extends Vue {
   @Prop({default: () => ({})}) readonly appConfig!: object
   @Prop({default: () => ({})}) readonly builderConfig!: object
 
-  flow = null
+  flowContainer = null
   uploadOrPasteSetting = "upload"
   flowJsonText = ""
+//[
+  //{
+    //"id": "22",
+    //"name": "English",
+    //"abbreviation": "EN",
+    //"orgId": "1008107874829627392",
+    //"rightToLeft": false,
+    //"code": null,
+    //"deletedAt": null
+  //},
+  //{
+    //"id": "23",
+    //"name": "Spanish",
+    //"abbreviation": "ES",
+    //"orgId": "1008107874829627392",
+    //"rightToLeft": false,
+    //"code": null,
+    //"deletedAt": null
+  //}
+//]
+  
+//[
+  //{
+    //"id": "22",
+    //"name": "English",
+    //"abbreviation": "EN",
+    //"orgId": "1008107874829627392",
+    //"rightToLeft": false,
+    //"code": null,
+    //"deletedAt": null
+  //}
+//]
+
+  validationErrors = {}
 
   get uploadOrPaste () {
     return this.uploadOrPasteSetting
   }
   set uploadOrPaste (value) {
+    if(this.uploadOrPasteSetting === 'upload' && value === 'paste') {
+      this.flowJsonText = ""
+    }
     this.uploadOrPasteSetting = value
   }
 
@@ -94,17 +157,68 @@ class ImportFlow extends Vue {
   }
   set flowJson(value) {
     this.flowJsonText = value
+    //check valid json
+    try {
+      this.flowContainer = JSON.parse(value)
+    } catch (e) {
+      this.flowError = "flow-builder.invalid-json-provided"
+      return
+    }
+    //this.checkIfValidFlow(this.flow) //TODO - what should we use here?
+    if(!this.checkSingleFlowOnly(this.flowContainer)) {
+      this.flowError = 'flow-builder.importer-currently-supports-single-flow-only'
+      return
+    }
+    this.validateLanguages(this.flowContainer)
+    //check languages
+    //check props
+    //check groups
+  }
+
+  checkSingleFlowOnly(flowContainer) {
+    console.log(flowContainer)
+    if(flowContainer.flows.length !== 1) {
+      return false
+    }
+    return true
+  }
+
+  validateLanguages(flowContainer) {
+    const missingLanguages = []
+    const matchingLanguages = []
+    const existingLanguagesWithoutMatch = cloneDeep(this.languages)
+    get(flowContainer, 'flows[0].languages', []).forEach((language) => {
+      if(!includes(this.languages, language)) {
+        missingLanguages.push(language)
+      } else {
+        matchingLanguages.push(language)
+      }
+      const existingLanguagesWithoutMatch = differenceWith(this.languages, matchingLanguages, isEqual);
+    })
   }
 
   get flowUUID() {
-    return get(this.flow, 'uuid')
+    return get(this.flowContainer, 'flows[0].uuid')
   }
 
+  async handleFileUpload(event) {
+    const selectedFile = event.target.files[0]
+
+    const reader = new FileReader()
+
+    reader.onload = (readEvent) => {
+      this.flowJson = readEvent.target.result.toString()
+    }
+
+    const contents = reader.readAsText(selectedFile, "UTF-8")
+  }
   async handleImportFlow(route) {
     this.flowError = null
-    const flowContainer = await this.flow_importFlow({
-      flow: this.flow
-    })
+    //TODO - ensure UUIDs generated?
+    //const flowContainer = await this.flow_importFlow({
+    //TODO - is this flow or flow container?
+      //flow: this.flow
+    //})
     if(flowContainer) {
       this.$router.push(route)
     } else {
@@ -118,6 +232,7 @@ class ImportFlow extends Vue {
   @flowVuexNamespace.Action flow_importFlow!: ({flow: IFlow}) => Promise<IContext>
   @Mutation configure 
   @Getter isConfigured!: boolean
+  @Getter languages!: ILanguage[] 
 }
 
 export default ImportFlow 
