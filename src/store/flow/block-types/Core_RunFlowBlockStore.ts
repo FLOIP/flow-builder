b4f28dac1c6644b465ba0793630eb2f6d9d56035
safydy <r.safydy@gmail.com>
2021-04-21 12:24:50 -0600

    VMO-3553 fix runFlow config case

--- src/store/flow/block-types/Core_RunFlowBlockStore.ts
+++ src/store/flow/block-types/Core_RunFlowBlockStore.ts
@@ -24,7 +24,7 @@ export const mutations: MutationTree<IFlowsState> = {
 
 export const actions: ActionTree<IFlowsState, IRootState> = {
   async setDestinationFlowId({ commit }, { blockId, newDestinationFlowId }: {blockId: string; newDestinationFlowId: string}) {
-    commit('flow/block_updateConfig', { blockId, newConfig: { flowId: newDestinationFlowId } }, { root: true })
+    commit('flow/block_updateConfig', { blockId, newConfig: { flow_id: newDestinationFlowId } }, { root: true })
     return newDestinationFlowId
   },
   async createWith({ dispatch }, { props }: {props: {uuid: string} & Partial<IRunFlowBlock>}) {
@@ -51,7 +51,7 @@ export const actions: ActionTree<IFlowsState, IRootState> = {
       label: '',
       semantic_label: '',
       config: {
-        flowId: '',
+        flow_id: '',
       },
       exits,
     })
