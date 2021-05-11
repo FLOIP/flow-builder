2918a71cccb8e08029f260e361fd58e45bc58cad
Roan Cooney <roan.cooney@viamo.io>
2021-05-11 12:42:44 -0600

    eslint-upgraded-and-fixed - heavily revamped ESLint. There were a bunch of issues, almost all the rules were disabled due to mistakes in how ESLint + vue + Typescript works. That means there is a lot to clean up in this project. I've set all the rules up, such that they match the best practices for TS and for Vue. The project will light up like a christmas tree until a ESLint auto fix is run on the project, but that is

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,29 +2,265 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    'jest/globals': true,
   },
+
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2020,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+
   extends: [
-    'plugin:vue/essential',
+    // TODO: Once we upgrade to Vue3, we need to change this to 'plugin:vue/vue3-recommended'
+    'plugin:vue/recommended',
+
     '@vue/airbnb',
     '@vue/typescript/recommended',
+
+    'plugin:jest/recommended',
+    'plugin:lodash/recommended',
+    'eslint:recommended',
+
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
   ],
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
-  rules: {
-    semi: [2, 'never'],
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-  },
+
+  plugins: [
+    'jest',
+    'lodash',
+    '@typescript-eslint',
+  ],
   overrides: [
     {
       files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+        '*.vue',
+        '*.js',
+        '*.ts',
+        '*.tsx',
       ],
+      excludedFiles: '*.png',
       env: {
         jest: true,
       },
     },
   ],
+
+  rules: {
+    /********************
+     * Typescript rules *
+     ********************/
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/strict-boolean-expressions': [
+      'error',
+      {
+        allowNullableBoolean: true,
+      },
+    ],
+    // Disabled until we can take the time to fix uses of object
+    '@typescript-eslint/ban-types': 'off',
+
+    // Disabled until we can take the time to fix uses
+    '@typescript-eslint/restrict-template-expressions': 'off',
+
+    '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/no-unsafe-return': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/semi': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/no-object-literal-type-assertion': 'off',
+    '@typescript-eslint/require-await': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
+    // Might be able to get rid of this after TS 4.0
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/explicit-function-return-type': [
+      'warn',
+      {
+        allowExpressions: true,
+        allowTypedFunctionExpressions: true,
+        allowHigherOrderFunctions: true,
+      },
+    ],
+    '@typescript-eslint/member-delimiter-style': [
+      'warn',
+      {
+        multiline: {
+          delimiter: 'comma',
+          requireLast: true,
+        },
+        singleline: {
+          delimiter: 'comma',
+          requireLast: false,
+        },
+      },
+    ],
+
+    /*************************
+     * Modified ESLint rules *
+     *************************/
+    'one-var': ['error', 'never'],
+
+    // These are off due to https://github.com/typescript-eslint/typescript-eslint/blob/f335c504bcf75623d2d671e2e784b047e5e186b9/docs/getting-started/linting/FAQ.md#eslint-plugin-import
+    'import/named': 'off',
+    'import/namespace': 'off',
+    'import/default': 'off',
+    'import/export': 'off',
+    'import/no-named-as-default-member': 'off',
+    'import/no-named-as-default': 'off',
+    'import/no-cycle': 'off',
+    'import/no-unused-modules': 'off',
+    'import/no-deprecated': 'off',
+    'import/no-absolute-path': 'off',
+    'no-useless-constructor': 'off',
+    'class-methods-use-this': 'off',
+    'no-empty-function': 'off',
+    'no-tabs': 'error',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-console': [
+      process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      {
+        allow: [
+          'warn',
+          'error',
+          'info',
+        ],
+      },
+    ],
+    'no-use-before-define': [
+      'off',
+      {
+        functions: true,
+        classes: true,
+        variables: false,
+      },
+    ],
+
+    'lines-between-class-members': ['off'],
+    'prefer-destructuring': [
+      'error',
+      {object: false, array: false},
+    ],
+    'space-before-function-paren': [
+      'error', {
+        anonymous: 'always',
+        named: 'never',
+        asyncArrow: 'always',
+      }],
+
+    'object-curly-spacing': [
+      'error',
+      'never',
+    ],
+
+    'object-curly-newline': [
+      'error',
+      {
+        ObjectExpression: {
+          multiline: true,
+          consistent: true,
+        },
+        ObjectPattern: {
+          multiline: true,
+          consistent: true,
+        },
+        ImportDeclaration: {
+          multiline: true,
+          consistent: true,
+        },
+        ExportDeclaration: {
+          multiline: true,
+          minProperties: 3,
+        },
+      },
+    ],
+
+    indent: [
+      'off',
+      2,
+      {
+        VariableDeclarator: 'off',
+        MemberExpression: 1,
+        ArrayExpression: 2,
+        ObjectExpression: 'off',
+        CallExpression: {arguments: 2},
+        FunctionDeclaration: {parameters: 2},
+        FunctionExpression: {parameters: 2},
+      },
+    ],
+    'no-trailing-spaces': [
+      'error',
+      {skipBlankLines: true},
+    ],
+    'max-len': [
+      'warn',
+      {code: 140},
+    ],
+    'eol-last': [
+      'warn',
+      'always',
+    ],
+    semi: [
+      'warn',
+      'never',
+    ],
+    'no-param-reassign': 'off',
+    'import/no-unresolved': ['off'],
+    'implicit-arrow-linebreak': 'off',
+
+    // Argument for this best presented here: https://youtu.be/eEBOvqMfPoI?t=1665
+    'no-else-return': ['off'],
+    'spaced-comment': 'off',
+    'line-comment-position': ['error', {position: 'above'}],
+
+    /**********
+     * Lodash *
+     **********/
+    'lodash/import-scope': [
+      'error',
+      'member',
+    ],
+    'lodash/matches-prop-shorthand': 'off',
+
+    // Disable all lodash prefer checks for things that already exist natively
+    'lodash/prefer-lodash-method': 'off',
+    'lodash/prefer-lodash-typecheck': 'off',
+    'lodash/prefer-noop': 'off',
+    'lodash/prefer-constant': 'off',
+    'lodash/preferred-alias': 'off',
+    'lodash/prefer-includes': 'off',
+    'lodash/prefer-get': 'off',
+    'lodash/prefer-is-nil': 'off',
+    'lodash/prefer-lodash-chain': 'off',
+    'lodash/prefer-matches': 'off',
+    'lodash/prefer-over-quantifier': 'off',
+    'lodash/prefer-some': 'off',
+    'lodash/prefer-startswith': 'off',
+    'lodash/prefer-times': 'off',
+
+    // this is as warning, as it breaks type inference
+    'lodash/prop-shorthand': ['warn', 'never'],
+
+    /**************
+     * Jest Rules *
+     **************/
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+  },
 }
