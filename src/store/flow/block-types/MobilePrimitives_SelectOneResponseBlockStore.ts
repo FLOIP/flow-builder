4c35ef41310a7164089a0c8ccef84a2ac7dce699
safydy <r.safydy@gmail.com>
2021-05-19 13:14:20 -0600

    VMO-3616 fix select many response block

--- src/store/flow/block-types/MobilePrimitives_SelectOneResponseBlockStore.ts
+++ src/store/flow/block-types/MobilePrimitives_SelectOneResponseBlockStore.ts
@@ -189,7 +189,7 @@ export const actions: ActionTree<ICustomFlowState, IRootState> = {
       ],
       config: {
         prompt: blankPromptResource.uuid,
-        prompt_audio: {},
+        prompt_audio: '',
         question_prompt: blankQuestionPromptResource.uuid,
         choices_prompt: blankChoicesPromptResource.uuid,
         choices: {},
