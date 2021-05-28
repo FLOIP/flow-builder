9c6fef5341ea963af13ef49dfa688268fb3080e6
safydy <r.safydy@gmail.com>
2021-05-27 22:24:25 -0600

    VMO-3943 prepare buttons

--- src/components/interaction-designer/toolbar/SelectionBanner.vue
+++ src/components/interaction-designer/toolbar/SelectionBanner.vue
@@ -2,9 +2,38 @@
   <div class="selection-banner" v-if="!!countSelectedBlocks">
     <div class="alert alert-primary mb-0 d-flex" role="alert">
       <div class="mr-3">
-        {{ 'flow-builder.selected-x-blocks' | trans({ x: countSelectedBlocks }) }}
+        {{ 'flow-builder.selected-x-blocks' | trans({ x: countSelectedBlocks }) }}.
       </div>
-      <button class="btn btn-outline-dark btn-sm" @click="block_clearMultiSelection">
+      <div class="mr-3" v-if="deleting">
+        <b>Are you sure you want to delete your selection ?</b>
+      </div>
+
+      <button
+        class="btn btn-danger btn-sm"
+        v-if="!deleting"
+        @click="deleting = true">
+        <font-awesome-icon
+          :icon="['far', 'trash-alt']"
+          class="fa-btn"
+        />
+        {{ 'flow-builder.delete' | trans }}
+      </button>
+
+      <button class="btn btn-outline-dark btn-sm mr-2"
+              v-if="deleting"
+              @click="deleting = false">
+        No, cancel
+      </button>
+
+      <button class="btn btn-danger btn-sm mr-2"
+              v-if="deleting"
+              @click="">
+        Yes, delete
+      </button>
+
+      <button class="btn btn-outline-dark btn-sm ml-4"
+              v-if="!deleting"
+              @click="block_clearMultiSelection">
         {{ 'flow-builder.clear-selection' | trans }}
       </button>
     </div>
@@ -21,9 +50,14 @@ const flowVuexNamespace = namespace('flow')
 
 @Component({})
 export default class SelectionBanner extends mixins(Lang) {
+  deleting: boolean = false
 
   get countSelectedBlocks() {
     return size(this.selectedBlocks)
+  }
+
+  confirmMultipleDeletion() {
+    this.deleting = true
   }
 
   @flowVuexNamespace.State selectedBlocks!: IBlock['uuid'][]
