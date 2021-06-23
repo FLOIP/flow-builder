df2735d57498c655d0d8549f2ab0c0999b4ffed0
Jacob Ashdown <jcbashdown@gmail.com>
2021-06-23 10:23:13 +0200

    VMO-3917 - intermediate multiselect solution

--- src/components/interaction-designer/flow-editors/import/LanguageAdder.vue
+++ src/components/interaction-designer/flow-editors/import/LanguageAdder.vue
@@ -22,11 +22,7 @@
         <validation-message message-key="language/new_language/iso_639_3" #input-control="{ isValid }">
           <label class="form-check-label mt-2 mb-2 mr-2">ISO 639 3 Code</label>
           <input name="iso_639_3" type="text" class="form-control full-width" v-model="newLanguage.iso_639_3">
-
-  <!-- Instead, use vue-multiselect? -->
-          <language-tag-selector
-              :audioFiles="availableAudio"
-              selectedAudioFile=""/>
+          <language-tag-selector/>
         </validation-message>
       </div>
       <div class="form-group">
@@ -65,7 +61,7 @@ import { IdGeneratorUuidV4 } from '@floip/flow-runner/dist/domain/IdGeneratorUui
 
 import ValidationMessage from '@/components/common/ValidationMessage.vue';
 import TextEditor from '@/components/common/TextEditor.vue'
-import LanguageTagSelector from '@/components/common/AudioLibrarySelector.vue'
+import LanguageTagSelector from '@/components/interaction-designer/flow-editors/import/LanguageTagSelector.vue'
 
 @Component({
   components: {
---
+++ src/components/interaction-designer/flow-editors/import/LanguageTagSelector.vue
@@ -0,0 +1,47 @@
+<template>
+  <!--<input name="iso_639_3" type="text" class="form-control full-width" v-model="newLanguage.iso_639_3">-->
+  <validation-message message-key="language/new_language/iso_639_3" #input-control="{ isValid }">
+    <label class="form-check-label mt-2 mb-2 mr-2">ISO 639 3 Code</label>
+    <vue-multiselect v-model="selectedTag"
+                     track-by="id"
+                     label="displayLabel"
+                     :class="{invalid: isValid === false}"
+                     :placeholder="'flow-builder.language-tag-selector-placeholder' | trans"
+                     :options="[]"
+                     :allow-empty="false"
+                     :show-labels="false"
+                     :searchable="true">
+    </vue-multiselect>
+  </validation-message>
+</template>
+
+<script lang="ts">
+import VueMultiselect from 'vue-multiselect';
+import { Component } from 'vue-property-decorator';
+import Lang from '@/lib/filters/lang';
+import { mixins } from "vue-class-component";
+import ValidationMessage from '@/components/common/ValidationMessage.vue';
+
+@Component<any>({
+  components: {
+    VueMultiselect,
+    ValidationMessage
+  },
+})
+class LanguageTagSelector extends mixins(Lang) {
+  get selectedTag() {
+    return ""
+  }
+
+  set selectedTag(value: string) {
+  }
+}
+
+export default LanguageTagSelector
+</script>
+
+<style lang="css" scoped>
+.invalid >>> .multiselect__tags {
+  border-color: #dc3545;
+}
+</style>
