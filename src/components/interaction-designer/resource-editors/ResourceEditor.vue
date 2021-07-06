d7cd50b62f0b0b35ccbaf872e48a5c187e076127
Pooja Patil <pooja.patil@viamo.io>
2021-07-06 12:32:36 -0700

    VMO-4138 Updated Resource editor

--- src/components/interaction-designer/resource-editors/ResourceEditor.vue
+++ src/components/interaction-designer/resource-editors/ResourceEditor.vue
@@ -6,87 +6,90 @@
       {{ label }}
     </h4>
 
-    <template v-for="{id: languageId, label: language} in flow.languages">
-      <div class="block-content-editor-lang">
-        <h5 class="badge badge-info">
-          {{ language || 'flow-builder.unknown-language' | trans }}
-        </h5>
-      </div>
+    <div>
+      <b-tabs>
+        <b-tab
+          v-for="{id: languageId, label: language} in flow.languages"
+          :key="languageId"
+          :title="language || 'flow-builder.unknown-language' | trans"
+          active>
+          <div v-for="(mode, i) in flow.supported_modes" :key="i" class="tab-content-style">
+            <h6>{{ `flow-builder.${mode.toLowerCase()}-content` | trans }}</h6>
 
-      <template v-for="mode in flow.supported_modes">
-        <h6>{{ `flow-builder.${mode.toLowerCase()}-content` | trans }}</h6>
+            <template v-for="contentType in discoverContentTypesFor(mode)">
+              <!-- todo: it's odd that we pass around a ContentType variant rather than a ContentTypeLangMode variant (aka, mode as external arg) -->
 
-        <template v-for="contentType in discoverContentTypesFor(mode)">
-          <!-- todo: it's odd that we pass around a ContentType variant rather than a ContentTypeLangMode variant (aka, mode as external arg) -->
+              <resource-variant-text-editor
+                v-if="contentType === SupportedContentType.TEXT"
+                :resource-id="resource.uuid"
 
-          <resource-variant-text-editor
-            v-if="contentType === SupportedContentType.TEXT"
-            :resource-id="resource.uuid"
+                :resource-variant="findOrGenerateStubbedVariantOn(
+                  resource,
+                  {language_id: languageId, content_type: contentType, modes: [mode]})"
+                :mode="mode"
+                :enable-autogen-button="true || enableAutogenButton" />
 
-            :resource-variant="findOrGenerateStubbedVariantOn(
-              resource,
-              {language_id: languageId, content_type: contentType, modes: [mode]})"
-            :mode="mode"
-            :enable-autogen-button="true || enableAutogenButton" />
+              <div v-if="contentType === SupportedContentType.AUDIO">
+                <template v-if="!findAudioResourceVariantFor(resource, {language_id: languageId, content_type: contentType, modes: [mode]})">
+                  <upload-monitor :upload-key="`${block.uuid}:${languageId}`" />
 
-          <div v-if="contentType === SupportedContentType.AUDIO">
-            <template v-if="!findAudioResourceVariantFor(resource, {language_id: languageId, content_type: contentType, modes: [mode]})">
-              <upload-monitor :upload-key="`${block.uuid}:${languageId}`" />
+                  <ul class="nav nav-tabs">
+                    <li class="nav-item">
+                      <a
+                        class="nav-link px-2 py-1 active"
+                        href="#"
+                        @click.prevent="">{{ 'flow-builder.library' | trans }}</a>
+                    </li>
+                    <li
+                      v-if="can(['edit-content', 'send-call-to-records'], true) && isFeatureAudioUploadEnabled"
+                      class="nav-item">
+                      <a
+                        href="#"
+                        class="nav-link px-2 py-1"
+                        @click.prevent="triggerRecordViaPhoneFor(languageId)">{{ 'flow-builder.phone-recording' | trans }}</a>
+                    </li>
 
-              <ul class="nav nav-tabs">
-                <li class="nav-item">
-                  <a
-                    class="nav-link px-2 py-1 active"
-                    href="#"
-                    @click.prevent="">{{ 'flow-builder.library' | trans }}</a>
-                </li>
-                <li
-                  v-if="can(['edit-content', 'send-call-to-records'], true) && isFeatureAudioUploadEnabled"
-                  class="nav-item">
-                  <a
-                    href="#"
-                    class="nav-link px-2 py-1"
-                    @click.prevent="triggerRecordViaPhoneFor(languageId)">{{ 'flow-builder.phone-recording' | trans }}</a>
-                </li>
+                    <li
+                      v-if="isEditable"
+                      class="nav-item">
+                      <a
+                        v-if="isFeatureAudioUploadEnabled"
+                        v-flow-uploader="{
+                          target: route('trees.resumeableAudioUpload'),
+                          token: `${block.uuid}${languageId}`,
+                          accept: 'audio/*'}"
+                        class="nav-link px-2 py-1"
+                        href="#"
+                        @filesSubmitted="handleFilesSubmittedFor(`${block.uuid}:${languageId}`, $event)"
+                        @fileSuccess="handleFileSuccessFor(`${block.uuid}:${languageId}`, languageId, $event)">
+                        {{ 'flow-builder.upload' | trans }}
+                      </a>
+                    </li>
+                  </ul>
+                </template>
 
-                <li
-                  v-if="isEditable"
-                  class="nav-item">
-                  <a
-                    v-if="isFeatureAudioUploadEnabled"
-                    v-flow-uploader="{
-                      target: route('trees.resumeableAudioUpload'),
-                      token: `${block.uuid}${languageId}`,
-                      accept: 'audio/*'}"
-                    class="nav-link px-2 py-1"
-                    href="#"
-                    @filesSubmitted="handleFilesSubmittedFor(`${block.uuid}:${languageId}`, $event)"
-                    @fileSuccess="handleFileSuccessFor(`${block.uuid}:${languageId}`, languageId, $event)">
-                    {{ 'flow-builder.upload' | trans }}
-                  </a>
-                </li>
-              </ul>
+                <audio-library-selector
+                  :audio-files="availableAudio"
+                  :lang-id="languageId"
+                  :resource-id="resource.uuid"
+                  :selected-audio-file="findOrGenerateStubbedVariantOn(
+                    resource,
+                    {language_id: languageId, content_type: contentType, modes: [mode]}).value" />
+
+                <phone-recorder
+                  v-if="can(['edit-content', 'send-call-to-records'], true) && !findOrGenerateStubbedVariantOn(resource,{language_id: languageId, content_type: contentType, modes: [mode]}).value"
+                  :recording-key="`${block.uuid}:${languageId}`" />
+              </div>
             </template>
-
-            <audio-library-selector
-              :audio-files="availableAudio"
-              :lang-id="languageId"
-              :resource-id="resource.uuid"
-              :selected-audio-file="findOrGenerateStubbedVariantOn(
-                resource,
-                {language_id: languageId, content_type: contentType, modes: [mode]}).value" />
-
-            <phone-recorder
-              v-if="can(['edit-content', 'send-call-to-records'], true) && !findOrGenerateStubbedVariantOn(resource,{language_id: languageId, content_type: contentType, modes: [mode]}).value"
-              :recording-key="`${block.uuid}:${languageId}`" />
           </div>
-        </template>
-      </template>
-    </template>
+        </b-tab>
+      </b-tabs>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
+import Vue from 'vue'
 import {Getter, Mutation, namespace} from 'vuex-class'
 import {
   IBlock,
@@ -112,8 +115,11 @@ import {ValidationException} from '@floip/flow-runner/src/domain/exceptions/Vali
 import PhoneRecorder from '@/components/interaction-designer/block-editors/PhoneRecorder.vue'
 import {ILanguage} from '@floip/flow-runner/dist/flow-spec/ILanguage'
 import {mixins} from 'vue-class-component'
+import {TabsPlugin} from 'bootstrap-vue'
 import UploadMonitor from '../block-editors/UploadMonitor.vue'
 import ResourceVariantTextEditor from './ResourceVariantTextEditor.vue'
+
+Vue.use(TabsPlugin)
 
 const flowVuexNamespace = namespace('flow')
 const builderVuexNamespace = namespace('builder')
@@ -227,3 +233,10 @@ export class ResourceEditor extends mixins(FlowUploader, Permissions, Routes, La
 
 export default ResourceEditor
 </script>
+
+<style scoped>
+.tab-content-style {
+  background: #F4F4F4;
+  padding: 10px;
+}
+</style>
