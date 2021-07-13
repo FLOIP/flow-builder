9dc0b1c597fff0049ddd59b0d598d04b75fb1f7c
Jacob Ashdown <jcbashdown@gmail.com>
2021-07-13 19:16:10 +0200

    VMO-4281 - add canvas_coordinates

--- src/store/flow/block.ts
+++ src/store/flow/block.ts
@@ -62,9 +62,6 @@ export const mutations: MutationTree<IFlowsState> = {
   block_updateConfigByPath(state, {blockId, path, value}: { blockId: string, path: string, value: object | string }) {
     set(findBlockOnActiveFlowWith(blockId, state as unknown as IContext).config!, path, value)
   },
-  block_updateVendorMetadataByPath(state, {blockId, path, value}: { blockId: string, path: string, value: object | string }) {
-    set(findBlockOnActiveFlowWith(blockId, state as unknown as IContext).vendor_metadata!, path, value)
-  },
   block_setBlockExitDestinationBlockId(state, {blockId, exitId, destinationBlockId}) {
     const block = findBlockOnActiveFlowWith(blockId, state as unknown as IContext)
     findBlockExitWith(exitId, block)
