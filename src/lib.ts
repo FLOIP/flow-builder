d7c59df9feb4f1d51999bb2f97c45ff5e0a1018e
Jacob Ashdown <jcbashdown@gmail.com>
2021-07-19 15:57:34 +0200

    VMO-4246 - reimport icons for use in integration

--- src/lib.ts
+++ src/lib.ts
@@ -5,6 +5,7 @@ import NewFlowComponent from './views/NewFlow.vue'
 import ImportFlowComponent from './views/ImportFlow.vue'
 import HomeComponent from './views/Home.vue'
 import defaultLocalisationsJson from "./assets/messages.json";
+import CustomIconComponents from './lib/custom-icons/index'
 
 export const appConfig = require('../app.config')
 export const builderConfig = require('../builder.config')
@@ -15,6 +16,7 @@ export const NewFlow = NewFlowComponent
 export const ImportFlow = ImportFlowComponent
 export const Home = HomeComponent
 export const defaultLocalisations = defaultLocalisationsJson
+export const CustomIcons = CustomIconComponents
 
 const Components: { [key: string]: any } = {
   InteractionDesignerComponent,
