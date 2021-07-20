57c08228e3e52ef1dbcd9d8f54c273a29563df82
Jacob Ashdown <jcbashdown@gmail.com>
2021-07-20 13:47:58 +0200

    VMO-4246 - pass query string to fetch - enable versioning

--- src/views/FetchFlow.vue
+++ src/views/FetchFlow.vue
@@ -55,10 +55,12 @@ class FetchFlow extends mixins(Routes, Lang) {
   @Getter isConfigured!: boolean
 
   async mounted() {
-    const flowContainer = await this.flow_fetch({fetchRoute: this.route('flows.fetchFlowServer', {flowId: this.uuid})})
+    const nextUrl: RawLocation = this.$route.query.nextUrl as RawLocation
+    const url = new URL(nextUrl as string, window.location.origin)
+    const urlParams = `?${url.searchParams.toString()}`
+    const flowContainer = await this.flow_fetch({fetchRoute: this.route('flows.fetchFlowServer', {flowId: this.uuid}) + urlParams})
     if (flowContainer) {
       this.flow_setActiveFlowId({flowId: this.uuid})
-      const nextUrl: RawLocation = this.$route.query.nextUrl as RawLocation
       if (nextUrl) {
         await this.$router.replace(nextUrl)
       } else {
