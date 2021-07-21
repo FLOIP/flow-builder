0fd32a9252355d0776a75dc60a05979b4d31b5a9
safydy <r.safydy@gmail.com>
2021-07-21 15:01:19 -0600

    VMO-4323 correct ui_metadata when creating block

--- src/store/builder/index.ts
+++ src/store/builder/index.ts
@@ -377,13 +377,12 @@ export function generateConnectionLayoutKeyFor(source: IBlock, target: IBlock) {
 }
 
 export function computeBlockUiData(block?: IBlock | null) {
-  const xDelta = 160
-  const yDelta = 180
+  const xDelta = 120
+  const yDelta = 110
+  let xPosition = block ? block.ui_metadata.canvas_coordinates.x : null
+  let yPosition = block ? block.ui_metadata.canvas_coordinates.y : null
 
-  let xPosition = get(block, 'block.ui_metadata.canvas_coordinates.x')
-  let yPosition = get(block, 'block.ui_metadata.canvas_coordinates.y')
-
-  if (!xPosition || !yPosition) {
+  if (xPosition === null || yPosition === null) {
     const viewPortCenter = getViewportCenter()
     xPosition = viewPortCenter.x
     yPosition = viewPortCenter.y
@@ -394,6 +393,7 @@ export function computeBlockUiData(block?: IBlock | null) {
     y: yPosition + yDelta,
   }
 }
+
 export function computeBlockVendorUiData(block?: IBlock | null) {
   return {
     isSelected: false,
