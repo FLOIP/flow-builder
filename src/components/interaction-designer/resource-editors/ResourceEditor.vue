0af6a8910d6d4174459755d64c48027a0b7999ab
Jacob Ashdown <jcbashdown@gmail.com>
2021-07-21 16:54:25 +0200

    VMO-4310 - update based on discussion

--- src/components/interaction-designer/resource-editors/ResourceEditor.vue
+++ src/components/interaction-designer/resource-editors/ResourceEditor.vue
@@ -69,6 +69,16 @@
                         class="fa-btn" />
                       {{ 'flow-builder.upload-audio' | trans }}
                     </button>
+                    <!-- TODO - VMO-4321 - leaving this here for moving over to embedding app when we are ready -->
+                    <!--<button
+                      v-if="can(['edit-content', 'send-call-to-records'], true) && isFeatureAudioUploadEnabled"
+                      class="btn btn-primary ivr-buttons ml-2"
+                      @click.prevent="triggerRecordViaPhoneFor(languageId)">
+                      <font-awesome-icon
+                        :icon="['fac', 'record-audio']"
+                        class="fa-btn" />
+                      {{ 'flow-builder.record-audio' | trans }}
+                    </button>-->
                   </div>
                 </template>
               </div>
