f2cbac94a298efc0543aa6b040324e99601b0fc5
Jacob Ashdown <jcbashdown@gmail.com>
2021-08-17 14:33:26 +0100

    VMO-3917 - fix patching

--- scripts/prepare-gh-pages-deployment.sh
+++ scripts/prepare-gh-pages-deployment.sh
@@ -16,7 +16,7 @@ abort()
 ***     $ git diff src/router/index.ts > patches/gh-pages-prepare-router.patch
 ***     $ git diff vue.config.js > patches/gh-pages-prepare-vue-config.patch
 ***     $ git diff builder.config.json > patches/remove-backend-routes-from-config.patch
-***     $ git diff > patches/disable-save-button.patch
+***     $ git diff builder.config.json > patches/disable-save-button.patch
 *** - commit changes for future usage
 *****************************************************************************************
 '
