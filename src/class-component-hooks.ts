a53eb935bb2213e5d4e838745e5164a970a5afce
Bulat Gabidullin <bulat.gabidullin@viamo.io>
2022-06-14 12:35:39 +0200

    VMO-5873 Export scrollBlockIntoView helper. Return Components from registerCustomComponents() to fix default export in lib.ts

--- src/class-component-hooks.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from 'vue-property-decorator'
-
-// See also https://github.com/vuejs/vue-class-component/issues/261
-Component.registerHooks([
-  'beforeRouteEnter',
-  'beforeRouteLeave',
-  'beforeRouteUpdate',
-])
