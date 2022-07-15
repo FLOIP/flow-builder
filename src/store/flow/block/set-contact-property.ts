d10125b95a4cd393d4000fc179a618a092ef1ed2
Nikolai Kim <nikolai.kim@viamo.io>
2022-07-15 14:25:18 +0700

    VMO-6461 Implement PR CRs

--- src/store/flow/block/set-contact-property.ts
+++ src/store/flow/block/set-contact-property.ts
@@ -3,30 +3,35 @@ import {findBlockOnActiveFlowWith as findBlock, IBlock, IContext, SetContactProp
 import {ActionTree, MutationTree} from 'vuex'
 import {IFlowsState} from '../index'
 
-export interface ISetContactPropertyIndex {
+export interface ISetContactPropertyBlockKey {
   blockId: IBlock['uuid'],
   key: SetContactProperty['property_key'],
 }
 
-export interface ISetContactPropertyIndexWithValue extends ISetContactPropertyIndex {
+export interface ISetContactPropertyBlockKeyWithValue extends ISetContactPropertyBlockKey {
   value: SetContactProperty['property_value'],
 }
 
 export const mutations: MutationTree<IFlowsState> = {
-  block_addContactProperty(state, {blockId, key, value}: ISetContactPropertyIndexWithValue) {
+  /**
+   * Adds a new contact_property to a specific block.
+   *
+   * @throws {Error} When a property with given key already exists
+   */
+  block_addContactProperty(state, {blockId, key, value}: ISetContactPropertyBlockKeyWithValue) {
     const block = findBlock(blockId, state as unknown as IContext)
 
-    // Ensure the block has a set_contact_property[]
     if (!block.config.set_contact_property) {
       block.config.set_contact_property = []
     }
 
-    // We should avoid duplicates
     if (block.config.set_contact_property.find(({property_key}) => property_key === key)) {
       throw new Error(`set_contact_property[${key}] already exists`)
     }
 
     block.config.set_contact_property = [
+      // Prepending because we currently display the foremost property only
+      // TODO VMO-6509 Push instead for better UX when we displayed all properties
       {
         property_key: key,
         property_value: value,
@@ -35,14 +40,14 @@ export const mutations: MutationTree<IFlowsState> = {
     ]
   },
 
-  block_removeContactProperty(state, {blockId, key}: ISetContactPropertyIndex) {
+  block_removeContactProperty(state, {blockId, key}: ISetContactPropertyBlockKey) {
     const block = findBlock(blockId, state as unknown as IContext)
 
     block.config.set_contact_property = (block.config.set_contact_property ?? [])
       .filter(({property_key}) => property_key !== key)
   },
 
-  block_changeContactProperty(state, {blockId, key, value: newValue}: ISetContactPropertyIndexWithValue) {
+  block_changeContactProperty(state, {blockId, key, value: newValue}: ISetContactPropertyBlockKeyWithValue) {
     const block = findBlock(blockId, state as unknown as IContext)
 
     block.config.set_contact_property = (block.config.set_contact_property ?? [])
@@ -54,15 +59,15 @@ export const mutations: MutationTree<IFlowsState> = {
 }
 
 export const actions: ActionTree<IFlowsState, IRootState> = {
-  block_getContactPropertyByKey(state, {blockId, key}: ISetContactPropertyIndex) {
+  block_getContactPropertyByKey(state, {blockId, key}: ISetContactPropertyBlockKey): string | undefined {
     return findBlock(blockId, state as unknown as IContext)
       .config
       .set_contact_property
       ?.find(({property_key}) => property_key === key)
-      ?.property_value ?? undefined
+      ?.property_value
   },
 
-  block_setContactPropertyByKey({commit, dispatch}, {blockId, key, value}: ISetContactPropertyIndexWithValue) {
+  block_setContactPropertyByKey({commit, dispatch}, {blockId, key, value}: ISetContactPropertyBlockKeyWithValue): void {
     if (dispatch('block_getContactPropertyByKey', {blockId, key}) === undefined) {
       commit('block_addContactProperty', {blockId, key, value})
     } else {
@@ -70,7 +75,7 @@ export const actions: ActionTree<IFlowsState, IRootState> = {
     }
   },
 
-  block_deleteContactPropertyByKey({commit}, {blockId, key}: ISetContactPropertyIndex) {
+  block_deleteContactPropertyByKey({commit}, {blockId, key}: ISetContactPropertyBlockKey): void {
     commit('block_removeContactProperty', {blockId, key})
   },
 }
