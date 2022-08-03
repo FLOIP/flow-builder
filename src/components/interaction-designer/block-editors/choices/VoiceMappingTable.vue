f1aebfcb65f1399388503f295c3129ee59c7d600
safydy <r.safydy@gmail.com>
2022-08-03 09:36:03 -0600

    VMO-6653 add row content

--- src/components/interaction-designer/block-editors/choices/VoiceMappingTable.vue
+++ src/components/interaction-designer/block-editors/choices/VoiceMappingTable.vue
@@ -9,9 +9,27 @@
     </thead>
     <tbody>
     <tr v-for="(choice, index) in block.config.choices" :key="index">
-      <td>{{ choice.name }}</td>
-      <td>one</td>
-      <td>one</td>
+      <td class="d-flex">
+        {{ choice.name }}
+        <div class="custom-control custom-checkbox ml-auto">
+          <input :id="`useExpression-${index}`" :name="`useExpression-${index}`" class="custom-control-input" type="checkbox">
+          <label :for="`useExpression-${index}`" class="custom-control-label">Use expression</label>
+        </div>
+      </td>
+      <td>
+        <div class="d-flex justify-content-center">
+          <select name="press-key">
+            <option name="one" value="1">1</option>
+            <option name="two" value="2">2</option>
+            <option name="three" value="3">3</option>
+            <option name="four" value="4">4</option>
+            <option name="star" value="*">*</option>
+          </select>
+        </div>
+      </td>
+      <td>
+        <input name="expression" type="text">
+      </td>
     </tr>
     </tbody>
   </table>
