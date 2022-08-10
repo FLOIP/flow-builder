3a570918e4dab1f6c03079ada3df3e086ab8c671
safydy <r.safydy@gmail.com>
2022-08-10 13:37:31 -0600

    VMO-6703 disable auto-suggest on choice builder

--- src/components/interaction-designer/resource-editors/ResourceVariantTextEditor.vue
+++ src/components/interaction-designer/resource-editors/ResourceVariantTextEditor.vue
@@ -11,6 +11,7 @@
           :current-expression="content"
           :rows="rows"
           :valid-state="isValid"
+          :disabled-auto-complete="disabledAutoComplete"
           @commitExpressionChange="commitExpressionChange" />
       </div>
     </template>
@@ -40,6 +41,7 @@ export class ResourceVariantTextEditor extends mixins(Lang) {
   @Prop() readonly resourceVariant!: IResourceValue
   @Prop() readonly mode!: SupportedMode
   @Prop({default: 2}) readonly rows!: number
+  @Prop({type: Boolean, default: false}) readonly disabledAutoComplete!: boolean
 
   get content(): string {
     return this.resourceVariant.value
