e1f6e95472b5331591d98fa66ac0b2b664bdf799
Bulat Gabidullin <bulat.gabidullin@viamo.io>
2022-09-12 18:23:51 +0200

    VMO-6919 add more console debugs

--- src/store/validation/validationHelpers.ts
+++ src/store/validation/validationHelpers.ts
@@ -253,17 +253,22 @@ export function getOrCreateResourceValidator(schemaVersion: string): ValidateFun
  * @param customBlockJsonSchema,
  */
 export function validateBlockWithJsonSchema({block, schemaVersion, customBlockJsonSchema}: {block: IBlock, schemaVersion: string, customBlockJsonSchema?: JSONSchema7}): IValidationStatus {
+  console.debug('validateBlockWithJsonSchema block, schemaVersion, customBlockJsonSchema:', block, schemaVersion, customBlockJsonSchema)
   let validate = null
+  console.debug('validateBlockWithJsonSchema validators:', validators)
   if (isEmpty(validators) || !validators.has(block.type)) {
-    const blockTypeWithoutNameSpace = block.type.split('.')[block.type.split('.').length - 1]
+    console.debug('validateBlockWithJsonSchema 1 if')
+    const blockTypeWithoutNameSpace = block.type.split('.').pop()
     let blockJsonSchema
     try {
       if (customBlockJsonSchema == null) {
         // eslint-disable-next-line import/no-dynamic-require,global-require
         blockJsonSchema = require(`@floip/flow-runner/dist/resources/validationSchema/${schemaVersion}/I${blockTypeWithoutNameSpace}Block.json`)
         validate = createDefaultJsonSchemaValidatorFactoryFor(blockJsonSchema)
+        console.debug('validateBlockWithJsonSchema 2 if blockJsonSchema, validate:', blockJsonSchema, validate)
       } else {
         validate = createDefaultJsonSchemaValidatorFactoryFor(customBlockJsonSchema)
+        console.debug('validateBlockWithJsonSchema 2 else validate:', validate)
       }
     } catch (e) {
       if (e.code === 'MODULE_NOT_FOUND') {
@@ -272,16 +277,19 @@ export function validateBlockWithJsonSchema({block, schemaVersion, customBlockJs
         // eslint-disable-next-line import/no-dynamic-require,global-require
         blockJsonSchema = require(`@floip/flow-runner/dist/resources/validationSchema/${schemaVersion}/flowSpecJsonSchema.json`)
         validate = createDefaultJsonSchemaValidatorFactoryFor(blockJsonSchema, '#/definitions/IBlock')
+        console.debug('validateBlockWithJsonSchema 3 if blockJsonSchema, validate:', blockJsonSchema, validate)
       } else {
+        console.debug('validateBlockWithJsonSchema 3 else')
         throw e
       }
     }
     validators.set(block.type, validate)
   } else {
+    console.debug('validateBlockWithJsonSchema 1 else')
     validate = validators.get(block.type)!
   }
 
-  return {
+  const res = {
     isValid: validate(block),
     ajvErrors: validate.errors,
     type: block.type,
@@ -290,4 +298,6 @@ export function validateBlockWithJsonSchema({block, schemaVersion, customBlockJs
       resourceUuid: get(block, 'config.prompt'),
     },
   }
+  console.debug('validateBlockWithJsonSchema res', res)
+  return res
 }
