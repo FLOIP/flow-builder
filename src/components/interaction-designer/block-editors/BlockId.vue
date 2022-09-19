58527170da17b3c7e6c12ac53ec50b3afad9cb68
safydy <r.safydy@gmail.com>
2022-09-19 15:13:21 -0600

    VMO-7017 fix trans mixin

--- src/components/interaction-designer/block-editors/BlockId.vue
+++ src/components/interaction-designer/block-editors/BlockId.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="block-id text-right">
-    <small class="text-muted">{{ lang.trans('flow-builder.block-id') }}: <em>{{ blockId }}</em></small>
+    <small class="text-muted">{{ trans('flow-builder.block-id') }}: <em>{{ blockId }}</em></small>
   </div>
 </template>
 
 <script lang="ts" setup>
 import {PropType, computed, defineProps} from 'vue'
 import {IBlock} from '@floip/flow-runner'
-import {lang} from '@/lib/filters/lang'
+import Lang from '@/lib/filters/lang'
 
 const props = defineProps({
   block: Object as PropType<IBlock>,
 })
 const blockId: string = computed(() => (props.block.uuid !== undefined ? props.block.uuid : ''))
+
+const {trans} = Lang
 </script>
