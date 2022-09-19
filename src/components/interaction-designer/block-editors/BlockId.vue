42e4aaee140b89163e7ba9dc2db3b76f2c36314a
safydy <r.safydy@gmail.com>
2022-09-19 12:23:38 -0600

    VMO-7017 use vue3 syntax (<script setup>)

--- src/components/interaction-designer/block-editors/BlockId.vue
+++ src/components/interaction-designer/block-editors/BlockId.vue
@@ -1,23 +1,16 @@
 <template>
   <div class="block-id text-right">
-    <small class="text-muted">{{ 'flow-builder.block-id' | trans }}: <em>{{ blockId }}</em></small>
+    <small class="text-muted">{{ lang.trans('flow-builder.block-id') }}: <em>{{ blockId }}</em></small>
   </div>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
+import {PropType, computed, defineProps} from 'vue'
 import {IBlock} from '@floip/flow-runner'
-import {Component, Prop} from 'vue-property-decorator'
-import Lang from '@/lib/filters/lang'
-import {mixins} from 'vue-class-component'
+import {lang} from '@/lib/filters/lang'
 
-@Component({})
-export class BlockId extends mixins(Lang) {
-  @Prop() readonly block!: IBlock
-
-  get blockId(): string {
-    return this.block.uuid || ''
-  }
-}
-
-export default BlockId
+const props = defineProps({
+  block: Object as PropType<IBlock>,
+})
+const blockId: string = computed(() => (props.block.uuid !== undefined ? props.block.uuid : ''))
 </script>
