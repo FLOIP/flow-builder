66c873e89ef10123649b484ef2d66bb73656ab4a
Nikolai Kim <nikolai.kim@viamo.io>
2022-09-22 18:24:09 +0700

    VMO-7069 Escape choices in Select One Response

--- src/components/interaction-designer/block-editors/choices/expressionTransformers.ts
+++ src/components/interaction-designer/block-editors/choices/expressionTransformers.ts
@@ -1,12 +1,16 @@
 import {IChoice} from '@floip/flow-runner'
 
+export function escapeQuotes(value: string): string {
+  return value.replace(/(['"])/g, '\\$1')
+}
+
 // eslint-disable-next-line import/prefer-default-export
 export function choicesToExpression(choices: IChoice[], propertyValueMapping: Record<string, string | number>): string {
   const individualLines = Object.entries(propertyValueMapping)
     .map(([choicePrompt, choiceValue]) => {
       const choiceName = choices.find(choice => choice.prompt === choicePrompt)?.name
       if (typeof choiceValue === 'string') {
-        return `IF(block.value = '${choiceName}', '${choiceValue}', '')`
+        return `IF(block.value = '${choiceName}', '${escapeQuotes(choiceValue)}', '')`
       } else {
         return `IF(block.value = '${choiceName}', ${choiceValue}, '')`
       }
