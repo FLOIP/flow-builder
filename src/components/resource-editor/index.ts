31ce85810fb6f4a8ca91cfda449ea695ae7655e5
safydy <r.safydy@gmail.com>
2022-09-28 22:59:52 -0600

    VMO-7102 clean unused resource-editor codes

--- src/components/resource-editor/index.ts
+++
@@ -1,4 +0,0 @@
-// TODO: remove all of these the phase 1 is done
-export * from './BatchMatchAudioFilesPrompt.vue'
-export * from './BlockContentEditorUnsupported.vue'
-export * from './HorizontalBlockContentEditor.vue'
