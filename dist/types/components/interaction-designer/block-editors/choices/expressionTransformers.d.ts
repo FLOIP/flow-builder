8a8d77e21f6c463acf4b8ff3f422670323c92520
safydy <r.safydy@gmail.com>
2022-10-05 15:53:14 -0600

    VMO-7069 build

--- dist/types/components/interaction-designer/block-editors/choices/expressionTransformers.d.ts
+++ dist/types/components/interaction-designer/block-editors/choices/expressionTransformers.d.ts
@@ -1,2 +1,3 @@
 import { IChoice } from '@floip/flow-runner';
+export declare function escapeQuotes(value: string): string;
 export declare function choicesToExpression(choices: IChoice[], propertyValueMapping: Record<string, string | number>): string;
