09d74e96807cb8eb9fb17cb2578cb7c473518290
safydy <r.safydy@gmail.com>
2022-10-27 15:15:01 -0600

    VMO-7272 build

--- dist/types/store/index.d.ts
+++ dist/types/store/index.d.ts
@@ -2,9 +2,11 @@ import { StoreOptions } from 'vuex';
 import { IClipboardState } from '../store/clipboard';
 import { IFlowsState } from './flow';
 import { IBuilderState } from './builder';
+import { IResourceViewerState } from './resource-viewer';
 import { IValidationState } from './validation';
 export interface IRootState {
     builder: IBuilderState;
+    resourceViewer: IResourceViewerState;
     flow: IFlowsState;
     validation: IValidationState;
     trees: any;
