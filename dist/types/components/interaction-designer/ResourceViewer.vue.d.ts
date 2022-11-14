ce73b31c19cbc0bbeb303c4a1a55cd1eaa8161b4
Nikolai Kim <nikolai.kim@viamo.io>
2022-11-14 13:02:57 -0600

    build-bundle

--- dist/types/components/interaction-designer/ResourceViewer.vue.d.ts
+++ dist/types/components/interaction-designer/ResourceViewer.vue.d.ts
@@ -4,9 +4,16 @@ export declare const DEBOUNCE_FLOW_PERSIST_MS = 1500;
 declare const _default: {
     name: string;
     mixins: (typeof Lang)[];
+    props: {
+        filteredBlocks: {
+            type: ArrayConstructor;
+            default: () => any[];
+        };
+    };
     computed: {
+        visibleBlocks(): any;
         id(): any;
-        filteredBlocks: import("vuex").Computed;
+        activeFlow: import("vuex").Computed;
     };
     mounted(): void;
     methods: {
