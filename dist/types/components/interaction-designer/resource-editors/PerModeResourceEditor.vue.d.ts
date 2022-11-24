9b06968ff678b342f49b247dc3cc89480567d411
Nikolai Kim <nikolai.kim@viamo.io>
2022-11-23 21:31:58 -0600

    build-bundle

--- dist/types/components/interaction-designer/resource-editors/PerModeResourceEditor.vue.d.ts
+++ dist/types/components/interaction-designer/resource-editors/PerModeResourceEditor.vue.d.ts
@@ -6,6 +6,7 @@ import FlowUploader from '../../../lib/mixins/FlowUploader';
 declare const PerModeResourceEditor_base: import("vue-class-component/lib/declarations").VueClass<Lang & Permissions & Routes & FlowUploader>;
 export declare class PerModeResourceEditor extends PerModeResourceEditor_base {
     block: IBlock;
+    openResources?: string[];
     activeFlow: IFlow;
     supportedModeWithOrderInfo: {
         mode: SupportedMode;
@@ -14,7 +15,11 @@ export declare class PerModeResourceEditor extends PerModeResourceEditor_base {
     }[];
     SupportedMode: typeof SupportedMode;
     iconsMap: Map<string, object>;
-    isPanelExpanded: {};
+    expandedPanels: {};
+    expandedPanelsSearch: {};
+    get isPanelExpanded(): {};
+    set isPanelExpanded(value: {});
+    overrideOpenResources(resources: string[]): void;
     updateIsPanelExpanded(id: any): void;
 }
 export default PerModeResourceEditor;
