4b35ea6f6968ed4a2878447e8facb31f65d08056
Bulat Gabidullin <bulat.gabidullin@viamo.io>
2022-12-09 23:26:48 +0100

    build-bundle

--- dist/types/store/flow/flow.d.ts
+++ dist/types/store/flow/flow.d.ts
@@ -1,8 +1,11 @@
-import { ILanguage } from '@floip/flow-runner';
+import { ILanguage, SupportedMode } from '@floip/flow-runner';
 import { ActionTree, GetterTree, MutationTree } from 'vuex';
 import { IRootState } from '../../store';
 import { IFlowsState } from '.';
-export declare function orderLanguages(LanguagesList: ILanguage[]): ILanguage[];
 export declare const getters: GetterTree<IFlowsState, IRootState>;
 export declare const mutations: MutationTree<IFlowsState>;
 export declare const actions: ActionTree<IFlowsState, IRootState>;
+/** Sort languages to display them in a particular order */
+export declare function orderLanguages(languages: ILanguage[]): ILanguage[];
+/** Sort modes to display them in a particular order */
+export declare function orderModes(modes: SupportedMode[]): SupportedMode[];
--- dist/types/store/flow/utils/resourceHelpers.d.ts
+++ dist/types/store/flow/utils/resourceHelpers.d.ts
@@ -1,12 +1,11 @@
 import { IBlock, IContext, IFlow, IResource, IResourceValue, IResourceValue as IResourceDefinitionVariantOverModes, SupportedContentType, SupportedMode } from '@floip/flow-runner';
 export declare type IResourceDefinitionVariantOverModesFilter = Partial<IResourceDefinitionVariantOverModes>;
 export declare type IResourceDefinitionVariantOverModesFilterAsKey = Omit<IResourceDefinitionVariantOverModes, 'value'>;
-export declare type IResourceDefinitionVariantOverModesFilterWithResourceId = Partial<IResourceDefinitionVariantOverModes> & {
-    resourceId: string;
-};
 export declare function findResourceWith(uuid: string, { resources }: IFlow): IResource;
 export declare function findResourceVariantOverModesWith(uuid: IResource['uuid'], filter: IResourceDefinitionVariantOverModesFilter, flow: IFlow): IResourceDefinitionVariantOverModes;
 export declare function findResourceVariantOverModesOn(resource: IResource, filter: IResourceDefinitionVariantOverModesFilter): IResourceValue;
+/** @returns resource variant's index in the resource.values array, -1 if not found */
+export declare function findIndexForResourceVariant(resource: IResource, filter: IResourceDefinitionVariantOverModesFilter): number;
 export declare function findOrGenerateStubbedVariantFor(resourceId: IResource['uuid'], filter: IResourceDefinitionVariantOverModesFilterAsKey, flow: IFlow): IResourceDefinitionVariantOverModes;
 export declare function findOrGenerateStubbedVariantOn(resource: IResource, filter: IResourceDefinitionVariantOverModesFilterAsKey): IResourceDefinitionVariantOverModes;
 export declare function discoverContentTypesFor(mode: SupportedMode, resource?: IResource): SupportedContentType[] | undefined;
@@ -15,10 +14,7 @@ export declare function findBlockRelatedResourcesUuids({ block }: {
     block: IBlock;
 }): IResource['uuid'][];
 /**
- * Compute resource index (cell index) for a table having X languages and Y modes
- *
- * @param languageIndex
- * @param modeIndex
- * @param supportedModesCount
+ * Extract resource variant index from error dataPath, e.g. `values/1/value` -> 1
+ * @param errorDataPath
  */
-export declare function computeResourceIndex(languageIndex: number, modeIndex: number, supportedModesCount: number): number;
+export declare function extractResourceVariantIndex(errorDataPath: string): number | undefined;
