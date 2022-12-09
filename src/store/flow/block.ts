65a7bb8e7a3c387937288241f06a503757409a66
Nikolai Kim <hello@nikolai.kim>
2022-12-09 16:53:55 -0600

    CORE-67 Allow appending exits (#404)
    * CORE-67 Add "append" option to block_addExit
    * CORE_67 Rename block_addExit parameter

--- src/store/flow/block.ts
+++ src/store/flow/block.ts
@@ -80,10 +80,14 @@ export const mutations: MutationTree<IFlowsState> = {
     const block = findBlockOnActiveFlowWith(blockId, state as unknown as IContext)
     findBlockExitWith(exitId, block).semantic_label = value
   },
-  block_addExit(state, {blockId, exit}: {blockId: string, exit: IBlockExit}) {
+  block_addExit(state, {blockId, exit, shouldAppend}: {blockId: string, exit: IBlockExit, shouldAppend?: boolean}) {
     const block = findBlockOnActiveFlowWith(blockId, state as unknown as IContext)
-    // insert before default exit
-    block.exits.splice(block.exits.length - 1, 0, exit)
+
+    if (shouldAppend) {
+      block.exits.push(exit)
+    } else {
+      block.exits.splice(block.exits.length - 1, 0, exit)
+    }
   },
 
   block_removeExit(state, {blockId, exit}: {blockId: string, exit: IBlockExit}) {
