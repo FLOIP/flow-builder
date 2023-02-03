9390dc7e9c534dffcf9cbf2c0cf5eaefe0d77f31
Bulat Gabidullin <bulat.gabidullin@viamo.io>
2023-02-03 16:48:30 +0100

    CORE-22 fix long line

--- src/store/flow/block.ts
+++ src/store/flow/block.ts
@@ -13,7 +13,13 @@ import {escapeQuotes} from '@/components/interaction-designer/block-editors/choi
 import {snakeCaseOnSpaces} from '@/utils/string-utils'
 import * as SetContactPropertyModule from './block/set-contact-property'
 import {IFlowsState} from '.'
-import {ConfigFieldType, removeBlockExitValueByPath, removeBlockValueByPath, updateBlockExitValueByPath, updateBlockValueByPath} from './utils/vuexBlockAndFlowHelpers'
+import {
+  ConfigFieldType,
+  removeBlockExitValueByPath,
+  removeBlockValueByPath,
+  updateBlockExitValueByPath,
+  updateBlockValueByPath,
+} from './utils/vuexBlockAndFlowHelpers'
 
 export const getters: GetterTree<IFlowsState, IRootState> = {
   ...SetContactPropertyModule.getters,
