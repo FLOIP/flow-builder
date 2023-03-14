32c0aa2f8b54ba4a3daed0dd08bc8080224f141e
safydy <r.safydy@gmail.com>
2023-03-14 11:51:02 -0600

    CORE-31 notify users when saving flow

---
+++ src/components/notification/InProgressAction.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="in-progress-action">
+    <font-awesome-icon
+      :icon="['fas', 'spinner']"
+      class="fa-btn fa-spin" />
+    {{textContent}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InProgressAction',
+  props: {
+    textContent: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
