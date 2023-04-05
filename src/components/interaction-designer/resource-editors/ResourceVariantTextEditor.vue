3c3271728bd56f34409a470b3b89ff7e64930622
Bulat Gabidullin <bulat.gabidullin@viamo.io>
2023-04-06 00:32:16 +0200

    CORE-352 add character count to SMS and USSD content editors

--- src/components/interaction-designer/resource-editors/ResourceVariantTextEditor.vue
+++ src/components/interaction-designer/resource-editors/ResourceVariantTextEditor.vue
@@ -1,22 +1,26 @@
 <template>
-  <validation-message
-    :message-key="`resource/${resourceId}/values/${index}/value`">
-    <template #input-control="{ isValid }">
-      <div class="resource-variant-text-editor mb-0">
+  <div class="resource-variant-text-editor mb-0">
+    <validation-message :message-key="`resource/${resourceId}/values/${index}/value`">
+      <template #input-control="{ isValid }">
         <expression-input
           ref="input"
           :label="label"
           :prepend-text="prependText"
-          :placeholder="placeholder || `flow-builder.enter-${resourceVariant.modes[0].replace('_', '-').toLowerCase()}-content` | trans"
+          :placeholder="pPlaceholder"
           :current-expression="content"
           :rows="rows"
           :valid-state="isValid"
           :disabled-auto-complete="disabledAutoComplete"
           class="mb-0"
-          @commitExpressionChange="commitExpressionChange"/>
-      </div>
-    </template>
-  </validation-message>
+          @commitExpressionChange="commitExpressionChange" />
+        <div
+          v-if="isSms || isUssd"
+          class="content-length-info">
+          {{ contentLengthInfo }}
+        </div>
+      </template>
+    </validation-message>
+  </div>
 </template>
 
 <script lang="ts">
@@ -38,14 +42,29 @@ export class ResourceVariantTextEditor extends mixins(Lang) {
   @Prop() readonly resourceId!: IResource['uuid']
   @Prop({default: ''}) readonly label!: string
   @Prop({type: String, default: ''}) readonly prependText!: string
-  @Prop() readonly placeholder!: string
+  @Prop() readonly placeholder: string
   @Prop() readonly resourceVariant!: IResourceValue
-  @Prop() readonly mode!: SupportedMode
+  @Prop() readonly mode: SupportedMode
   @Prop({default: 2}) readonly rows!: number
   @Prop({type: Boolean, default: false}) readonly disabledAutoComplete!: boolean
 
+  get pPlaceholder(): string {
+    return this.placeholder || this.trans(`flow-builder.enter-${this.mode.toLowerCase()}-content`)
+  }
+
   get content(): string {
     return this.resourceVariant.value
+  }
+
+  get contentLengthInfo(): string {
+    const charCount = this.content.length
+    const smsPagesCount = Math.ceil(this.content.length / 160)
+
+    if (smsPagesCount > 1 && this.isSms) {
+      return this.trans('flow-builder.x-characters-y-pages', {charCount, smsPagesCount})
+    } else {
+      return this.trans('flow-builder.x-characters', {charCount})
+    }
   }
 
   commitExpressionChange(value: string): void {
@@ -71,9 +90,24 @@ export class ResourceVariantTextEditor extends mixins(Lang) {
     this.$emit('afterResourceVariantChanged', {variant: resourceVariant, resourceId, value})
   }
 
+  get isSms(): boolean {
+    return this.mode === SupportedMode.SMS
+  }
+
+  get isUssd(): boolean {
+    return this.mode === SupportedMode.USSD
+  }
+
   @flowVuexNamespace.Action resource_setOrCreateValueModeSpecific!:
     ({resourceId, filter, value}: { resourceId: IResource['uuid'], filter: {}, value: string }) => void
 }
 
 export default ResourceVariantTextEditor
 </script>
+
+<style scoped lang="scss">
+@import "../../../scss/custom_variables";
+.content-length-info {
+  color: $neutral-900;
+}
+</style>
