6349764eb6f28fbb9e69c3c98f606b4a62d22734
Lakh Bawa <lakhveer.bawa@viamo.io>
2023-05-01 14:17:53 -0400

    CORE-215/CORE-483 added test for sibling flow behaviour

--- tests/unit/flow-import/flowImport.spec.ts
+++ tests/unit/flow-import/flowImport.spec.ts
@@ -60,4 +60,35 @@ describe('create_flow_stack', () => {
     const flowStack = createContainerFlowStack(contextObject)
     expect(flowStack).toEqual(['flow3', 'flow2', 'flow4', 'flow1', 'flow5'])
   })
+
+  test('should create a flow stack with correct order_when_there_are_sibling_flows', () => {
+    const jsonData = `
+      {
+        "flows": [
+          {
+            "uuid": "flow3",
+            "blocks": []
+          },
+          {
+            "uuid": "flow4",
+            "blocks": []
+          },
+          {
+            "uuid": "flow5",
+            "blocks": []
+          }
+        ]
+      }
+    `
+    const jsonDataParsed = JSON.parse(jsonData)
+
+    const contextObject = {
+    name: 'TODO',
+    description: 'TODO',
+    vendor_metadata: {},
+    flows: jsonDataParsed.flows,
+  } as unknown as IContext
+    const flowStack = createContainerFlowStack(contextObject)
+    expect(flowStack).toEqual(['flow3', 'flow4', 'flow5'])
+  })
 })
