fc020beb4b57dfd268cb071c51805a1f39d174f4
Bulat Gabidullin <bulat.gabidullin@viamo.io>
2023-05-18 00:39:24 +0200

    CORE-541 undoable block code/name editing

--- src/components/interaction-designer/block-editors/NameEditor.vue
+++ src/components/interaction-designer/block-editors/NameEditor.vue
@@ -6,12 +6,13 @@
           <h6>{{ trans('flow-builder.edit-block-code') }}</h6>
           <div class="d-flex">
             <text-editor
-              v-model="blockName"
               class="w-100"
               :label="''"
               :placeholder="trans('flow-builder.enter-block-code')"
               :valid-state="isValid"
-              @keydown="filterName" />
+              :value="blockName"
+              @keydown="filterName"
+              @input="setBlockName" />
             <span
               class="btn btn-primary btn-xs align-self-center ml-2"
               @click="handleCompleteEditing">
@@ -47,10 +48,11 @@
 import {Component, Prop} from 'vue-property-decorator'
 import {mixins} from 'vue-class-component'
 import Lang from '@/lib/filters/lang'
-import {IBlock, IBlockUIMetadata} from '@floip/flow-runner'
+import {IBlock} from '@floip/flow-runner'
 import {namespace} from 'vuex-class'
 
 const flowVuexNamespace = namespace('flow')
+const undoRedoVuexNamespace = namespace('undoRedo')
 
 @Component({})
 export class NameEditor extends mixins(Lang) {
@@ -62,12 +64,13 @@ export class NameEditor extends mixins(Lang) {
     return this.block.name
   }
 
-  set blockName(value: IBlock['name']) {
-    this.block_setName({
+  async setBlockName(value: IBlock['name']): Promise<void> {
+    await this.block_setName({
       blockId: this.block.uuid,
       value,
       lockAutoUpdate: true,
     })
+    await this.takeSnapshot()
   }
 
   filterName(e: KeyboardEvent): void {
@@ -96,9 +99,10 @@ export class NameEditor extends mixins(Lang) {
     blockId: IBlock['uuid'],
     value: IBlock['name'],
     lockAutoUpdate: boolean,
-  }) => void;
+  }) => Promise<void>
 
-  @flowVuexNamespace.Action block_resetName!: ({blockId}: { blockId: IBlock['uuid'] }) => void;
+  @flowVuexNamespace.Action block_resetName!: ({blockId}: { blockId: IBlock['uuid'] }) => void
+  @undoRedoVuexNamespace.Action takeSnapshot!: () => Promise<void>
 }
 export default NameEditor
 </script>
