f21d2bb67aa8299206d821427c28dd43632ecf13
Bulat Gabidullin <bulat.gabidullin@viamo.io>
2023-05-23 14:46:48 +0200

    CORE-539 try-catch block duplication

--- src/components/interaction-designer/block/BlockToolbar.vue
+++ src/components/interaction-designer/block/BlockToolbar.vue
@@ -94,13 +94,17 @@ export class BlockToolbar extends mixins(Lang) {
   }
 
   async handleDuplicateBlock(): Promise<void> {
-    const duplicatedBlock = await this.flow_duplicateBlock({blockId: this.block.uuid})
-    await this.$router.replace({
-      name: 'block-selected-details',
-      params: {blockId: duplicatedBlock.uuid},
-    })
-    await this.takeSnapshot()
-    this.$emit('after-duplicate')
+    try {
+      const duplicatedBlock = await this.flow_duplicateBlock({blockId: this.block.uuid})
+      await this.$router.replace({
+        name: 'block-selected-details',
+        params: {blockId: duplicatedBlock.uuid},
+      })
+      await this.takeSnapshot()
+      this.$emit('after-duplicate')
+    } catch (e) {
+      console.error('Failed to duplicate a block:', e)
+    }
   }
 
   @builderVuexNamespace.Getter isEditable !: boolean
