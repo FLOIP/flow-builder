4b4cc19611afcb64c1c44c32cdb73b0e6af13ed3
Nikolai Kim <nikolai.kim@viamo.io>
2023-05-25 14:19:08 -0600

    CORE-567 Fix typing errors

--- src/store/undoRedo/getChangedKeys.ts
+++ src/store/undoRedo/getChangedKeys.ts
@@ -1,14 +1,14 @@
 /* eslint-disable import/prefer-default-export, @typescript-eslint/no-explicit-any */
-export function getChangedKeys(a: any = {}, b: any = {}): string[] {
+export function getChangedKeys(a: object, b: object): string[] {
   const changedKeys: string[] = []
 
-  Object.keys({...a as object, ...b as object})
+  Object.keys({...a, ...b})
     .forEach((key) => {
-      const aVal = (a as Record<string, any>)[key]
-      const bVal = (b as Record<string, any>)[key]
+      const aVal = (a as Record<string, unknown>)[key]
+      const bVal = (b as Record<string, unknown>)[key]
 
-      if (typeof aVal === 'object' && typeof bVal === 'object') {
-        changedKeys.push(...getChangedKeys(aVal as object, bVal as object).map((k) => `${key}.${k}`))
+      if (typeof aVal === 'object' && typeof bVal === 'object' && aVal !== null && bVal !== null) {
+        changedKeys.push(...getChangedKeys(aVal, bVal).map((k) => `${key}.${k}`))
       } else if (aVal !== bVal) {
         changedKeys.push(key)
       }
