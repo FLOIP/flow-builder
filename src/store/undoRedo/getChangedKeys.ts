aefd5d1aeec7e47ad12956f00bae856674799cd5
Nikolai Kim <nikolai.kim@viamo.io>
2023-06-06 15:26:37 -0600

    CORE-578 Diff newly created objects

--- src/store/undoRedo/getChangedKeys.ts
+++ src/store/undoRedo/getChangedKeys.ts
@@ -7,12 +7,23 @@ export function getChangedKeys(a: object, b: object): string[] {
       const aVal = (a as Record<string, unknown>)[key]
       const bVal = (b as Record<string, unknown>)[key]
 
-      if (typeof aVal === 'object' && typeof bVal === 'object' && aVal !== null && bVal !== null) {
-        changedKeys.push(...getChangedKeys(aVal, bVal).map((k) => `${key}.${k}`))
+      if (isObjectOrUndefined(aVal) && isObjectOrUndefined(bVal)) {
+        changedKeys.push(...getChangedKeys(
+          ensureObject(aVal),
+          ensureObject(bVal),
+        ).map((k) => `${key}.${k}`))
       } else if (aVal !== bVal) {
         changedKeys.push(key)
       }
     })
 
   return changedKeys
+}
+
+function ensureObject(obj: any): object {
+  return obj !== null && obj !== undefined ? obj : {}
+}
+
+function isObjectOrUndefined(obj: any): boolean {
+  return obj === undefined || typeof obj === 'object'
 }
