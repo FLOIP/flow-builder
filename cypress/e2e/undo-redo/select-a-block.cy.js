0b23054be4f17338e5daf521af4158624dcc22c6
Nikolai Kim <nikolai.kim@viamo.io>
2023-06-13 23:28:39 -0600

    CORE-629 Selecting a block WIP

---
+++ cypress/e2e/undo-redo/select-a-block.cy.js
@@ -0,0 +1,16 @@
+describe('adding blocks', () => {
+  beforeEach(() => {
+    cy.createFlow({
+      label: 'selecting blocks test',
+    })
+  })
+
+  it('adds three blocks, selects the 2nd', () => {
+    const foo = cy.addBlock(['Content', 'Message'])
+    cy.addBlock(['Branching'])
+
+    foo.then((uuid) => {
+      cy.selectBlock(uuid).should('equal', uuid)
+    })
+  })
+})
