39f8fd78e9526695a7fe9d25b5f520e51c425215
Nikolai Kim <nikolai.kim@viamo.io>
2023-06-14 11:24:50 -0600

    CORE-629 Saving flows test

---
+++ cypress/e2e/undo-redo/save-a-flow.cy.js
@@ -0,0 +1,20 @@
+describe('saving a flow', () => {
+    beforeEach(() => {
+      cy.createFlow({
+        label: 'saving flows test',
+      })
+    })
+  
+    it('should be disabled when there are no changes', () => {
+      cy.get('[data-cy="save--btn"]').should('be.disabled')
+    })
+  
+    it('is working when there are changes', () => {
+        cy.addBlock(['Content', 'Message'])
+        cy.get('[data-cy="save--btn"]')
+            .should('not.be.disabled')
+            .click()
+        cy.get('[data-cy="save--btn"]').should('be.disabled')
+    })
+  })
+  
