f36e3fafe46ef582496b3272f7a96c6790ab0063
Nikolai Kim <nikolai.kim@viamo.io>
2023-06-15 16:55:04 -0600

    build-bundle

---
+++ dist/types/components/interaction-designer/toolbar/UndoRedoButtonGroup.vue.d.ts
@@ -0,0 +1,2 @@
+declare const _default: import("vue").DefineComponent<{}, {}, {}, {}, {}, import("vue/types/v3-component-options").ComponentOptionsMixin, import("vue/types/v3-component-options").ComponentOptionsMixin, {}, string, Readonly<import("vue").ExtractPropTypes<{}>>, {}>;
+export default _default;
