ceee1ddfa7e325d5d840398bb1768900e3208c0f
Nikolai Kim <nikolai.kim@viamo.io>
2023-06-21 08:01:39 -0600

    CORE-664 Add diff type indicator to getChangedKeys

--- src/store/undoRedo/__tests__/getChangedKeys.test.ts
+++ src/store/undoRedo/__tests__/getChangedKeys.test.ts
@@ -11,37 +11,37 @@ describe('getChangedKeys', () => {
   it('should return an array of changed keys when given two different objects', () => {
     const a = {foo: 1, bar: {baz: 2}}
     const b = {foo: 2, bar: {baz: 3}}
-    expect(getChangedKeys(a, b)).toEqual(['foo', 'bar.baz'])
+    expect(getChangedKeys(a, b)).toEqual(['foo~', 'bar.baz~'])
   })
 
   it('should handle objects with different keys', () => {
     const a = {foo: 1, bar: {baz: 2}}
     const b = {foo: 1, bar: {baz: 2, qux: 3}}
-    expect(getChangedKeys(a, b)).toEqual(['bar.qux'])
+    expect(getChangedKeys(a, b)).toEqual(['bar.qux+'])
   })
 
   it('should handle objects with null values', () => {
     const a = {foo: null}
     const b = {foo: 1}
-    expect(getChangedKeys(a, b)).toEqual(['foo'])
+    expect(getChangedKeys(a, b)).toEqual(['foo~'])
   })
 
   it('should handle objects with undefined values', () => {
     const a = {foo: 1}
     const b = {foo: undefined}
-    expect(getChangedKeys(a, b)).toEqual(['foo'])
+    expect(getChangedKeys(a, b)).toEqual(['foo-'])
   })
 
   it('should handle objects with arrays', () => {
     const a = {foo: [1, 2, 3]}
     const b = {foo: [1, 2, 4]}
-    expect(getChangedKeys(a, b)).toEqual(['foo.2'])
+    expect(getChangedKeys(a, b)).toEqual(['foo.2~'])
   })
 
   it('should handle objects with nested arrays', () => {
     const a = {foo: [[1, 2], [3, 4]]}
     const b = {foo: [[1, 2], [3, 5]]}
-    expect(getChangedKeys(a, b)).toEqual(['foo.1.1'])
+    expect(getChangedKeys(a, b)).toEqual(['foo.1.1~'])
   })
 
   it('should handle nested objects', () => {
@@ -63,6 +63,13 @@ describe('getChangedKeys', () => {
       },
     }
 
-    expect(getChangedKeys(a, b)).toEqual(['foo.bar.baz', 'foo.bar.boo', 'foo.bar.uwu'])
+    expect(getChangedKeys(a, b)).toEqual(['foo.bar.baz~', 'foo.bar.boo-', 'foo.bar.uwu+'])
+  })
+
+  it('should handle deleted array items', () => {
+    const a = {foo: {bar: [1, 2, 3]}}
+    const b = {foo: {bar: [1, 3]}}
+
+    expect(getChangedKeys(a, b)).toEqual(['foo.bar.1~', 'foo.bar.2-'])
   })
 })
