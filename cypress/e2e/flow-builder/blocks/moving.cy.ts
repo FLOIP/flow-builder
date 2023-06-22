37ef9484c85dc48bdfb04eaed6dc93a8a6a533dc
Bulat Gabidullin <bulat.gabidullin@viamo.io>
2023-06-22 15:26:21 +0200

    CORE-632 test block moving with cypress

---
+++ cypress/e2e/flow-builder/blocks/moving.cy.ts
@@ -0,0 +1,75 @@
+import {IBlock} from '@floip/flow-runner'
+import Chainable = Cypress.Chainable
+
+describe('Moving a block', () => {
+  beforeEach(() => {
+    cy.createFlow({
+      label: 'Moving a block',
+    })
+
+    cy.addBlock(['Content', 'Message']).then(blockId => {
+      cy.get(`[data-cy="block--${blockId}"]`)
+        .find('[data-cy^="block-handle--"]')
+        .as('blockArea')
+    })
+
+    // wait for flow validation to succeed and flow validation toolbar to disappear
+    // to avoid interference with block coordinates
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(200)
+  })
+
+  it('moves the block', () => {
+    const deltaX = 200
+    const deltaY = 300
+
+    // eslint-disable-next-line arrow-body-style
+    return getBlockVisualCoordinates().then(({x: oldVisualX, y: oldVisualY}) => {
+      return getBlockMetadataCoordinates().then(({x: oldMetadataX, y: oldMetadataY}) => {
+        cy.get('@blockArea').move({deltaX, deltaY})
+        // cy.wait(200)
+
+        // eslint-disable-next-line arrow-body-style
+        return getBlockVisualCoordinates().then(({x: newVisualX, y: newVisualY}) => {
+          return getBlockMetadataCoordinates().then(({x: newMetadataX, y: newMetadataY}) => {
+            cy.log('old visual x,y:', oldVisualX, oldVisualY)
+            cy.log('new visual x,y:', newVisualX, newVisualY)
+
+            cy.wrap(newVisualX - oldVisualX).should('eq', deltaX)
+            cy.wrap(newVisualY - oldVisualY).should('eq', deltaY)
+
+            cy.log('old metadata x,y:', oldMetadataX, oldMetadataY)
+            cy.log('new metadata x,y:', newMetadataX, newMetadataY)
+
+            cy.wrap(newMetadataX - oldMetadataX).should('eq', deltaX)
+            cy.wrap(newMetadataY - oldMetadataY).should('eq', deltaY)
+          })
+        })
+      })
+    })
+  })
+})
+
+function getBlockVisualCoordinates(): Chainable<{x: number, y: number}> {
+  return cy.get('[data-cy="builder-canvas--page"]').then(canvasEl => {
+    const {left: leftSidebarWidth, top: topToolbarHeight} = canvasEl[0].getBoundingClientRect()
+
+    return cy.get('@blockArea').then(block => {
+      const {left, top} = block[0].getBoundingClientRect()
+
+      const x = left - leftSidebarWidth
+      const y = top - topToolbarHeight
+
+      return {x, y}
+    })
+  })
+}
+
+function getBlockMetadataCoordinates(): Chainable<{x: number, y: number}> {
+  return cy.window()
+    .its('store.state.flow.flows[0].blocks[0]')
+    .then((oldState: IBlock) => {
+      const {x, y} = oldState.ui_metadata!.canvas_coordinates!
+      return {x, y}
+    })
+}
