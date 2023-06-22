6bb723f6e9db491955c9e4ee185bfc95078e1676
Nikolai Kim <nikolai.kim@viamo.io>
2023-06-22 12:35:40 -0600

    CORE-664 Fix key difference detection

--- cypress/e2e/flow-builder/undo-redo/actions-on-block.cy.js
+++ cypress/e2e/flow-builder/undo-redo/actions-on-block.cy.js
@@ -1,64 +1,88 @@
-describe('Add/Delete/Duplicate blocks', () => {
+describe('Actions on blocks', () => {
   beforeEach(() => {
     cy.createFlow({
       label: 'my test flow',
     })
   })
 
-  it('can undo/redo correctly', () => {
-    // #### Add a block
-    let messageBlockUuid = null
+  it('can undo/redo adding a block', () => {
     cy.addBlock(['Content', 'Message']).then((blockUuid) => {
-      messageBlockUuid = blockUuid
       cy.get(`[data-cy="block--${blockUuid}"]`).should('exist')
       cy.undo()
       cy.get(`[data-cy="block--${blockUuid}"]`).should('not.exist')
       cy.redo()
       cy.get(`[data-cy="block--${blockUuid}"]`).should('exist')
+    })
+  })
 
-      // TODO in CORE-664: enable the test after fixing the bug
-      // Block deletion, scenario1: Delete the block right away, with just one block in the snapshot
-      // cy.deleteBlock(messageBlockUuid).then(() => {
-      //   cy.get(`[data-cy="block--${messageBlockUuid}"]`).should('not.exist')
-      //   cy.undo()
-      //   cy.get(`[data-cy="block--${messageBlockUuid}"]`).should('exist')
-      //   cy.redo()
-      //   cy.get(`[data-cy="block--${messageBlockUuid}"]`).should('not.exist')
-      //
-      //   // TODO in CORE-664: bring next tests in this line once the bug is fixed
-      // })
+  it('can undo/redo deleting the first block', () => {
+    cy.addBlock(['Content', 'Message']).then((blockUuid) => {
+      cy.get(`[data-cy="block--${blockUuid}"]`).should('exist')
 
-      // #### Duplicate that block
-      let duplicatedMessageBlockUuid = null
-      cy.duplicateBlock(messageBlockUuid).then((blockUuid) => {
-        duplicatedMessageBlockUuid = blockUuid
-        cy.get(`[data-cy="block--${duplicatedMessageBlockUuid}"]`).should('exist')
+      cy.deleteBlock(blockUuid)
+      cy.get(`[data-cy="block--${blockUuid}"]`).should('not.exist')
+
+      cy.undo()
+      cy.get(`[data-cy="block--${blockUuid}"]`).should('exist')
+
+      cy.redo()
+      cy.get(`[data-cy="block--${blockUuid}"]`).should('not.exist')
+    })
+  })
+
+  it('can undo/redo deleting the last block', () => {
+    cy.addBlock(['Content', 'Message']).then((blockUuid) => {
+      cy.get(`[data-cy="block--${blockUuid}"]`).should('exist')
+
+      cy.addBlock(['Content', 'Message']).then((blockUuid2) => {
+        cy.get(`[data-cy="block--${blockUuid2}"]`).should('exist')
+
+        cy.deleteBlock(blockUuid)
+        cy.get(`[data-cy="block--${blockUuid}"]`).should('not.exist')
+
         cy.undo()
-        cy.get(`[data-cy="block--${duplicatedMessageBlockUuid}"]`).should('not.exist')
+        cy.get(`[data-cy="block--${blockUuid}"]`).should('exist')
+
         cy.redo()
-        cy.get(`[data-cy="block--${duplicatedMessageBlockUuid}"]`).should('exist')
+        cy.get(`[data-cy="block--${blockUuid}"]`).should('not.exist')
+      })
+    })
+  })
 
-        // #### Duplicate multiple blocks
-        cy.addBlock(['Content', 'Numeric Response']).then((blockUuid2) => {
-          cy.duplicateMultipleBlocks([duplicatedMessageBlockUuid, blockUuid2]).then((duplicatedBlocks) => {
-            cy.get(`[data-cy="block--${duplicatedBlocks[0]}"]`).should('exist')
-            cy.get(`[data-cy="block--${duplicatedBlocks[1]}"]`).should('exist')
-            cy.undo()
-            cy.get(`[data-cy="block--${duplicatedBlocks[0]}"]`).should('not.exist')
-            cy.get(`[data-cy="block--${duplicatedBlocks[1]}"]`).should('not.exist')
-            cy.redo()
-            cy.get(`[data-cy="block--${duplicatedBlocks[0]}"]`).should('exist')
-            cy.get(`[data-cy="block--${duplicatedBlocks[1]}"]`).should('exist')
+  it('can undo/redo duplicating a block', () => {
+    cy.addBlock(['Content', 'Message']).then((blockUuid) => {
+      cy.get(`[data-cy="block--${blockUuid}"]`).should('exist')
 
-            // #### Block deletion, scenario2: Delete a block, after we have few snapshots
-            cy.deleteBlock(duplicatedMessageBlockUuid).then(() => {
-              cy.get(`[data-cy="block--${duplicatedMessageBlockUuid}"]`).should('not.exist')
-              cy.undo()
-              cy.get(`[data-cy="block--${duplicatedMessageBlockUuid}"]`).should('exist')
-              cy.redo()
-              cy.get(`[data-cy="block--${duplicatedMessageBlockUuid}"]`).should('not.exist')
-            })
-          })
+      cy.duplicateBlock(blockUuid).then((duplicatedBlockUuid) => {
+        cy.get(`[data-cy="block--${duplicatedBlockUuid}"]`).should('exist')
+
+        cy.undo()
+        cy.get(`[data-cy="block--${duplicatedBlockUuid}"]`).should('not.exist')
+
+        cy.redo()
+        cy.get(`[data-cy="block--${duplicatedBlockUuid}"]`).should('exist')
+      })
+    })
+  })
+
+  it('can undo/redo duplicating multiple blocks', () => {
+    cy.addBlock(['Content', 'Message']).then((blockUuid) => {
+      cy.get(`[data-cy="block--${blockUuid}"]`).should('exist')
+
+      cy.addBlock(['Content', 'Numeric Response']).then((blockUuid2) => {
+        cy.get(`[data-cy="block--${blockUuid2}"]`).should('exist')
+
+        cy.duplicateMultipleBlocks([blockUuid, blockUuid2]).then((duplicatedBlocks) => {
+          cy.get(`[data-cy="block--${duplicatedBlocks[0]}"]`).should('exist')
+          cy.get(`[data-cy="block--${duplicatedBlocks[1]}"]`).should('exist')
+
+          cy.undo()
+          cy.get(`[data-cy="block--${duplicatedBlocks[0]}"]`).should('not.exist')
+          cy.get(`[data-cy="block--${duplicatedBlocks[1]}"]`).should('not.exist')
+
+          cy.redo()
+          cy.get(`[data-cy="block--${duplicatedBlocks[0]}"]`).should('exist')
+          cy.get(`[data-cy="block--${duplicatedBlocks[1]}"]`).should('exist')
         })
       })
     })
